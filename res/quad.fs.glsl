d51466340c521eddbe8faa51556373682004724d
Glenn Watson <gw@intuitionlibrary.com>
2015-09-22 15:28:58 +1000

    Fix alpha mask on android

--- res/quad.fs.glsl
+++ res/quad.fs.glsl
@@ -12,6 +12,12 @@ varying vec4 vColor;
 void main(void)
 {
 	vec4 diffuse = texture2D(sDiffuse, vColorTexCoord);
-	vec4 mask = vec4(1.0, 1.0, 1.0, texture2D(sMask, vMaskTexCoord).r);
+
+	#ifdef PLATFORM_ANDROID
+		vec4 mask = vec4(1.0, 1.0, 1.0, texture2D(sMask, vMaskTexCoord).a);
+	#else
+		vec4 mask = vec4(1.0, 1.0, 1.0, texture2D(sMask, vMaskTexCoord).r);
+	#endif
+
 	gl_FragColor = diffuse * vColor * mask;
 }
