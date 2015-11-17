7dafcf2a21138f55ac3af7f372d3c24cf902e13e
Glenn Watson <gw@intuitionlibrary.com>
2015-11-17 13:02:53 +1000

    Fix f32 uv coords for composite shaders

--- res/blend.vs.glsl
+++ res/blend.vs.glsl
@@ -1,6 +1,6 @@
 void main(void)
 {
-	vColorTexCoord = vec3(aColorTexCoord / 65535.0, 0.0);
+	vColorTexCoord = vec3(aColorTexCoord, 0.0);
 	vMaskTexCoord = vec3(aMaskTexCoord / 65535.0, 0.0);
     gl_Position = uTransform * vec4(aPosition, 1.0);
 }
