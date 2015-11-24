22daaa225236741f610360b81339e42b87fb79cb
Glenn Watson <gw@intuitionlibrary.com>
2015-11-24 14:06:22 +1000

    Add pipeline size change to render notifier trait.

--- src/types.rs
+++ src/types.rs
@@ -238,6 +238,9 @@ pub struct PipelineId(pub u32, pub u32);
 
 pub trait RenderNotifier : Send {
     fn new_frame_ready(&mut self);
+    fn pipeline_size_changed(&mut self,
+                             pipeline_id: PipelineId,
+                             size: Option<Size2D<f32>>);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
