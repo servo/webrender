48350aa396279171c1f3d54847256aa9a8ea23ff
Anthony Ramine <n.oxyde@gmail.com>
2016-06-28 14:44:47 +0200

    Use wrappers for GL IDs
    These wrappers make the handling of buffers, framebuffers, programs,
    renderbuffers, shaders and textures a bit more type-safe and allow
    us to make nonzero usage optional.

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#![cfg_attr(feature = "nightly", feature(nonzero))]
 #![cfg_attr(feature = "serde_macros", feature(custom_derive, plugin))]
 #![cfg_attr(feature = "serde_macros", plugin(serde_macros))]
-#![feature(nonzero)]
 
 extern crate app_units;
 extern crate byteorder;
+#[cfg(feature = "nightly")]
 extern crate core;
 extern crate euclid;
 extern crate gleam;
+extern crate heapsize;
 extern crate ipc_channel;
 extern crate offscreen_gl_context;
 extern crate serde;
