ec5d9f64fec3edc2f239a750a1ac6504d5a71368
lana <alanamramjit@gmail.com>
2016-08-03 16:29:21 -0700

    [replay] record scrolling and read from directory

--- replay/src/main.rs
+++ replay/src/main.rs
@@ -15,7 +15,8 @@ use std::io::{BufReader};
 use std::io::prelude::*;
 use std::path::PathBuf;
 use std::env;
-use webrender_traits::{ApiMsg, PipelineId};
+use webrender_traits::{ApiMsg, RenderApi, PipelineId};
+use glutin::{Event, ElementState, VirtualKeyCode as Key};
 
 struct Notifier {
 window_proxy: glutin::WindowProxy,
@@ -30,7 +31,6 @@ impl Notifier {
 
 impl webrender_traits::RenderNotifier for Notifier {
 	fn new_frame_ready(&mut self) {
-		println!("new frame ready");		
 		self.window_proxy.wakeup_event_loop();
 	}
 
@@ -42,33 +42,43 @@ impl webrender_traits::RenderNotifier for Notifier {
 	fn new_scroll_frame_ready(&mut self, composite_needed:bool){}
 }
 
-fn read_struct(reader: &mut BufReader<File>) -> Option<Vec<u8>>{
-	let len = reader.read_u32::<LittleEndian>().ok();
-	match len{
-		Some(len) => {
-			let mut buffer = vec![0; len as usize ];
-			reader.read_exact(&mut buffer).unwrap();
-			Some(buffer)
-		}
-		_ => None,
-	}
+
+fn get_file(dir:&String, frame: i32) -> Option<File>{
+	let filename = format!("{}/frame_{}.bin", dir,frame);
+	File::open(filename).ok()
 }
+
+fn read_file(file: &mut File, api: &RenderApi){	
+	while let Some(len) = file.read_u32::<LittleEndian>().ok(){
+		let mut buffer = vec![0; len as usize];
+		file.read_exact(&mut buffer).unwrap();
+		let msg:Option<ApiMsg> = deserialize(&buffer).ok();
+		match msg{
+			Some(msg) => {	
+				api.api_sender.send(msg).unwrap();
+			}
+			None => {
+				api.payload_sender.send(&buffer[..]).unwrap();
+			}
+		}
+	}
+}	
+
 
 fn main() {
 	let args:Vec<String> = env::args().collect();
-	if args.len() != 2{
-		println!("{}  <resources_path>", args[0]);
+	if args.len() != 3{
+		println!("{}  <resources_path> <directory>", args[0]);
 		return;
 	}
 	let resource_path = &args[1];
-	let resources = "resources.bin";
-	let display_lists = "display_list.bin";
+	let ref dir= args[2];
 	let window = glutin::WindowBuilder::new()
 		.with_gl(glutin::GlRequest::Specific(glutin::Api::OpenGl, (3,2)))
 		.build()
 		.unwrap();
 
-	let (width, height) = window.get_inner_size().unwrap();
+	let (width, height) = window.get_outer_size().unwrap();
 	unsafe{
 		window.make_current().ok();
 		gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
@@ -89,44 +99,42 @@ fn main() {
 	let (mut width, mut height) = window.get_outer_size().unwrap();
 	width *= window.hidpi_factor() as u32;
 	height *= window.hidpi_factor() as u32;
+
+
 	//read and send the resources file
-	let f = File::open("resources.bin").unwrap();
-	let mut reader = BufReader::new(f);
-
-	while let Some(buffer) = read_struct(&mut reader){
-		let msg:ApiMsg = deserialize(&buffer).unwrap();
-		api.api_sender.send(msg).unwrap()
-	}		
-
-	let f = File::open(display_lists).unwrap();
-	let mut reader = BufReader::new(f);
-	if let Some(buff) = read_struct(&mut reader){
-		let msg:ApiMsg = deserialize(&buff).unwrap();
-		api.api_sender.send(msg).unwrap();
-	}
-
-	if let Some(auxiliary_data) = read_struct(&mut reader){
-		api.payload_sender.send(&auxiliary_data[..]).unwrap();
-	}
+	let mut frame_num = 0;
+	if let Some(mut file) = get_file(dir, frame_num){
+		read_file(&mut file, &api);
+	}	
 	for event in window.wait_events(){
 		match event {
-			glutin::Event::Closed => break,
-				glutin::Event::Awakened => { 				
+			Event::Closed => break,
+			Event::Awakened => { 				
 					gl::clear(gl::COLOR_BUFFER_BIT);
 					renderer.update();
-					renderer.render(Size2D::new(width, height as u32));
+					renderer.render(Size2D::new(width, height));
 					window.swap_buffers();
 			}
-			glutin::Event::KeyboardInput(glutin::ElementState::Released, _, _) =>{
-				if let Some(buff) = read_struct(&mut reader){
-					let msg:ApiMsg = deserialize(&buff).unwrap();
-					api.api_sender.send(msg).unwrap();
+			Event::KeyboardInput(ElementState::Released, _, Some(Key::Right)) =>{
+				frame_num += 1;
+				if let Some(mut file) = get_file(dir, frame_num){
+					read_file(&mut file, &api);
+				}
+				else{
+					frame_num -= 1;
+					println!("At last frame.");
+				}	
+			}
+			Event::KeyboardInput(ElementState::Released, _, Some(Key::Left)) => {
+				frame_num -= 1;
+				if let Some(mut file) = get_file(dir, frame_num){
+					read_file(&mut file, &api);
 				}
 
-				if let Some(auxiliary_data) = read_struct(&mut reader){
-					api.payload_sender.send(&auxiliary_data[..]).unwrap();
+				else{
+					frame_num +=1;
+					println!("At first frame.");
 				}
-
 			}
 			_ => ()
 		}
