588e9631284625db80823758d9bdff326eb446bb
Anthony Ramine <n.oxyde@gmail.com>
2016-10-04 18:02:36 +0200

    Switch to serde_derive

--- webrender_traits/src/lib.rs
+++ webrender_traits/src/lib.rs
@@ -3,8 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #![cfg_attr(feature = "nightly", feature(nonzero))]
-#![cfg_attr(feature = "serde_macros", feature(custom_derive, plugin))]
-#![cfg_attr(feature = "serde_macros", plugin(serde_macros))]
+#![cfg_attr(feature = "serde_derive", feature(rustc_macro))]
 
 extern crate app_units;
 extern crate byteorder;
@@ -16,13 +15,16 @@ extern crate heapsize;
 extern crate ipc_channel;
 extern crate offscreen_gl_context;
 extern crate serde;
+#[cfg(feature = "serde_derive")]
+#[macro_use]
+extern crate serde_derive;
 
 #[cfg(target_os = "macos")] extern crate core_graphics;
 
 #[cfg(feature = "serde_codegen")]
 include!(concat!(env!("OUT_DIR"), "/types.rs"));
 
-#[cfg(feature = "serde_macros")]
+#[cfg(feature = "serde_derive")]
 include!("types.rs");
 
 mod api;
