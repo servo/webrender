464242486cc023279223ad2bbd7f0932e17ca92c
Sam <swood@mozilla.com>
2016-11-08 17:06:19 +0000

    Implement scroll in frame.rs and add required types

--- webrender_traits/src/types.rs
+++ webrender_traits/src/types.rs
@@ -50,7 +50,7 @@ pub enum ApiMsg {
                            Vec<(DisplayListId, BuiltDisplayListDescriptor)>,
                            AuxiliaryListsDescriptor),
     SetRootPipeline(PipelineId),
-    Scroll(Point2D<f32>, Point2D<f32>, ScrollEventPhase),
+    Scroll(ScrollLocation, Point2D<f32>, ScrollEventPhase),
     TickScrollingBounce,
     TranslatePointToLayerSpace(Point2D<f32>, IpcSender<(Point2D<f32>, PipelineId)>),
     GetScrollLayerState(IpcSender<Vec<ScrollLayerState>>),
@@ -437,6 +437,13 @@ pub struct ScrollLayerState {
 pub enum ScrollPolicy {
     Scrollable,
     Fixed,
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Serialize)]
+pub enum ScrollLocation {
+    Delta(Point2D<f32>), // Scroll by a certain amount.
+    Start, // Scroll to very top of element.
+    End // Scroll to very bottom of element.
 }
 
 #[derive(Clone, Copy, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
