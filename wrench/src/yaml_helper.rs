28bc050fe739e178cc8d55a484ffb396410ec26d
Nicolas Silva <nical@fastmail.com>
2016-11-30 22:58:43 +0100

    Use strongly typed units in wrench.

--- wrench/src/yaml_helper.rs
+++ wrench/src/yaml_helper.rs
@@ -2,7 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use euclid::{Size2D, Point2D, Rect, Matrix4D};
 use std::str::FromStr;
 use app_units::Au;
 
@@ -14,10 +13,10 @@ pub trait YamlHelper {
     fn as_force_f32(&self) -> Option<f32>;
     fn as_vec_f32(&self) -> Option<Vec<f32>>;
     fn as_vec_u32(&self) -> Option<Vec<u32>>;
-    fn as_rect(&self) -> Option<Rect<f32>>;
-    fn as_size(&self) -> Option<Size2D<f32>>;
-    fn as_point(&self) -> Option<Point2D<f32>>;
-    fn as_matrix4d(&self) -> Option<Matrix4D<f32>>;
+    fn as_rect(&self) -> Option<LayoutRect>;
+    fn as_size(&self) -> Option<LayoutSize>;
+    fn as_point(&self) -> Option<LayoutPoint>;
+    fn as_matrix4d(&self) -> Option<LayoutTransform>;
     fn as_colorf(&self) -> Option<ColorF>;
     fn as_complex_clip_rect(&self) -> Option<ComplexClipRegion>;
     fn as_clip_region(&self, &mut DisplayListBuilder) -> Option<ClipRegion>;
@@ -77,7 +76,7 @@ impl YamlHelper for Yaml {
         }
     }
 
-    fn as_rect(&self) -> Option<Rect<f32>> {
+    fn as_rect(&self) -> Option<LayoutRect> {
         if self.is_badvalue() {
             return None;
         }
@@ -86,10 +85,10 @@ impl YamlHelper for Yaml {
         if nums.len() != 4 {
             panic!("rect expected 4 float values, got {} instead ('{:?}')", nums.len(), self);
         }
-        Some(Rect::new(Point2D::new(nums[0], nums[1]), Size2D::new(nums[2], nums[3])))
+        Some(LayoutRect::new(LayoutPoint::new(nums[0], nums[1]), LayoutSize::new(nums[2], nums[3])))
     }
 
-    fn as_size(&self) -> Option<Size2D<f32>> {
+    fn as_size(&self) -> Option<LayoutSize> {
         if self.is_badvalue() {
             return None;
         }
@@ -98,10 +97,10 @@ impl YamlHelper for Yaml {
         if nums.len() != 2 {
             panic!("size expected 2 float values, got {} instead ('{:?}')", nums.len(), self);
         }
-        Some(Size2D::new(nums[0], nums[1]))
+        Some(LayoutSize::new(nums[0], nums[1]))
     }
 
-    fn as_point(&self) -> Option<Point2D<f32>> {
+    fn as_point(&self) -> Option<LayoutPoint> {
         if self.is_badvalue() {
             return None;
         }
@@ -110,10 +109,10 @@ impl YamlHelper for Yaml {
         if nums.len() != 2 {
             panic!("point expected 2 float values, got {} instead ('{:?}')", nums.len(), self);
         }
-        Some(Point2D::new(nums[0], nums[1]))
+        Some(LayoutPoint::new(nums[0], nums[1]))
     }
 
-    fn as_matrix4d(&self) -> Option<Matrix4D<f32>> {
+    fn as_matrix4d(&self) -> Option<LayoutTransform> {
         None
     }
 
@@ -146,23 +145,23 @@ impl YamlHelper for Yaml {
 
         let nums = self.as_vec_f32().unwrap();
         match nums.len() {
-            4 => Some(ComplexClipRegion::new(Rect::new(Point2D::new(nums[0], nums[1]), Size2D::new(nums[2], nums[3])),
+            4 => Some(ComplexClipRegion::new(LayoutRect::new(LayoutPoint::new(nums[0], nums[1]), LayoutSize::new(nums[2], nums[3])),
                                              BorderRadius::zero())),
-            5 => Some(ComplexClipRegion::new(Rect::new(Point2D::new(nums[0], nums[1]), Size2D::new(nums[2], nums[3])),
+            5 => Some(ComplexClipRegion::new(LayoutRect::new(LayoutPoint::new(nums[0], nums[1]), LayoutSize::new(nums[2], nums[3])),
                                              BorderRadius::uniform(nums[4]))),
-            8 => Some(ComplexClipRegion::new(Rect::new(Point2D::new(nums[0], nums[1]), Size2D::new(nums[2], nums[3])),
+            8 => Some(ComplexClipRegion::new(LayoutRect::new(LayoutPoint::new(nums[0], nums[1]), LayoutSize::new(nums[2], nums[3])),
                                              BorderRadius {
-                                                 top_left: Size2D::new(nums[4], nums[4]),
-                                                 top_right: Size2D::new(nums[5], nums[5]),
-                                                 bottom_left: Size2D::new(nums[6], nums[6]),
-                                                 bottom_right: Size2D::new(nums[7], nums[7]),
+                                                 top_left: LayoutSize::new(nums[4], nums[4]),
+                                                 top_right: LayoutSize::new(nums[5], nums[5]),
+                                                 bottom_left: LayoutSize::new(nums[6], nums[6]),
+                                                 bottom_right: LayoutSize::new(nums[7], nums[7]),
                                              })),
-            12 => Some(ComplexClipRegion::new(Rect::new(Point2D::new(nums[0], nums[1]), Size2D::new(nums[2], nums[3])),
+            12 => Some(ComplexClipRegion::new(LayoutRect::new(LayoutPoint::new(nums[0], nums[1]), LayoutSize::new(nums[2], nums[3])),
                                               BorderRadius {
-                                                  top_left: Size2D::new(nums[4], nums[5]),
-                                                  top_right: Size2D::new(nums[6], nums[7]),
-                                                  bottom_left: Size2D::new(nums[8], nums[9]),
-                                                  bottom_right: Size2D::new(nums[10], nums[11]),
+                                                  top_left: LayoutSize::new(nums[4], nums[5]),
+                                                  top_right: LayoutSize::new(nums[6], nums[7]),
+                                                  bottom_left: LayoutSize::new(nums[8], nums[9]),
+                                                  bottom_right: LayoutSize::new(nums[10], nums[11]),
                                               })),
             n => panic!("complex clip rect expected 4, 5, 8, or 12 floats; got {} instead at '{:?}'", n, self),
         }
@@ -183,7 +182,7 @@ impl YamlHelper for Yaml {
         }
 
         // otherwise it's an array of complex clip rects
-        let mut bounds = Rect::<f32>::zero();
+        let mut bounds = LayoutRect::zero();
         let mut clips = Vec::<ComplexClipRegion>::new();
 
         for item in self.as_vec().unwrap() {
