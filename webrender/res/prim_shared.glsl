cc725ae01b576b2c2b543f60de1f80e98bb1696a
Josh Matthews <josh@joshmatthews.net>
2017-01-11 16:36:07 -0500

    Work around an older mac shader compiler bug.

--- webrender/res/prim_shared.glsl
+++ webrender/res/prim_shared.glsl
@@ -3,11 +3,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#if defined(GL_ES) && GL_ES == 1
-    #ifdef GL_FRAGMENT_PRECISION_HIGH
-    precision highp sampler2DArray;
-    #else
-    precision mediump sampler2DArray;
+#if defined(GL_ES)
+    #if GL_ES == 1
+        #ifdef GL_FRAGMENT_PRECISION_HIGH
+        precision highp sampler2DArray;
+        #else
+        precision mediump sampler2DArray;
+        #endif
     #endif
 #endif
 
