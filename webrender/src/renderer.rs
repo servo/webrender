21876642a98af5157932013cb7664d43b74b1007
Lee Salzman <lsalzman@gmail.com>
2017-01-17 15:08:58 -0500

    add support for radial gradients

--- webrender/src/renderer.rs
+++ webrender/src/renderer.rs
@@ -57,6 +57,7 @@ const GPU_TAG_PRIM_COMPOSITE: GpuProfileTag = GpuProfileTag { label: "Composite"
 const GPU_TAG_PRIM_TEXT_RUN: GpuProfileTag = GpuProfileTag { label: "TextRun", color: debug_colors::BLUE };
 const GPU_TAG_PRIM_GRADIENT: GpuProfileTag = GpuProfileTag { label: "Gradient", color: debug_colors::YELLOW };
 const GPU_TAG_PRIM_ANGLE_GRADIENT: GpuProfileTag = GpuProfileTag { label: "AngleGradient", color: debug_colors::POWDERBLUE };
+const GPU_TAG_PRIM_RADIAL_GRADIENT: GpuProfileTag = GpuProfileTag { label: "RadialGradient", color: debug_colors::LIGHTPINK };
 const GPU_TAG_PRIM_BOX_SHADOW: GpuProfileTag = GpuProfileTag { label: "BoxShadow", color: debug_colors::CYAN };
 const GPU_TAG_PRIM_BORDER: GpuProfileTag = GpuProfileTag { label: "Border", color: debug_colors::ORANGE };
 const GPU_TAG_PRIM_CACHE_IMAGE: GpuProfileTag = GpuProfileTag { label: "CacheImage", color: debug_colors::SILVER };
@@ -307,6 +308,7 @@ pub struct Renderer {
     ps_border: PrimitiveShader,
     ps_gradient: PrimitiveShader,
     ps_angle_gradient: PrimitiveShader,
+    ps_radial_gradient: PrimitiveShader,
     ps_box_shadow: PrimitiveShader,
     ps_cache_image: PrimitiveShader,
 
@@ -473,6 +475,10 @@ impl Renderer {
                                                      &mut device,
                                                      &[],
                                                      options.precache_shaders);
+        let ps_radial_gradient = PrimitiveShader::new("ps_radial_gradient",
+                                                     &mut device,
+                                                     &[],
+                                                     options.precache_shaders);
         let ps_cache_image = PrimitiveShader::new("ps_cache_image",
                                                   &mut device,
                                                   &[],
@@ -625,6 +631,7 @@ impl Renderer {
             ps_box_shadow: ps_box_shadow,
             ps_gradient: ps_gradient,
             ps_angle_gradient: ps_angle_gradient,
+            ps_radial_gradient: ps_radial_gradient,
             ps_cache_image: ps_cache_image,
             ps_blend: ps_blend,
             ps_composite: ps_composite,
@@ -1009,6 +1016,10 @@ impl Renderer {
             &PrimitiveBatchData::AngleGradient(ref data) => {
                 let shader = self.ps_angle_gradient.get(&mut self.device, transform_kind);
                 (data, GPU_TAG_PRIM_ANGLE_GRADIENT, shader)
+            }
+            &PrimitiveBatchData::RadialGradient(ref data) => {
+                let shader = self.ps_radial_gradient.get(&mut self.device, transform_kind);
+                (data, GPU_TAG_PRIM_RADIAL_GRADIENT, shader)
             }
         };
 
