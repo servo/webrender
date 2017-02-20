578882623dc8b8e3baed9ff1bf6564af0390bc69
JerryShih <hshih@mozilla.com>
2017-02-21 02:55:40 +0800

    Simplify ExternalImageId type.
    The ExternalImageId only contains a u64 field. Use u64 directly instead of a struct.

--- webrender_traits/src/types.rs
+++ webrender_traits/src/types.rs
@@ -602,8 +602,7 @@ pub enum YuvColorSpace {
 /// An arbitrary identifier for an external image provided by the
 /// application. It must be a unique identifier for each external
 /// image.
-#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash, Serialize, Deserialize)]
-pub struct ExternalImageId(pub u64);
+pub type ExternalImageId = u64;
 
 #[derive(Clone, Serialize, Deserialize)]
 pub enum ImageData {
