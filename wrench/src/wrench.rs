4ee8d7d78fc905616b467e3b835dbe9bd48df080
Jeff Muizelaar <jmuizelaar@mozilla.com>
2017-02-24 16:50:32 -0500

    Change set_root_display_list to accept built display lists
    This will let us build our display lists in the child process in Gecko,
    transport them over Gecko IPC and send them into webrender in the
    compositor process.

--- wrench/src/wrench.rs
+++ wrench/src/wrench.rs
@@ -364,7 +364,7 @@ impl Wrench {
         self.api.set_root_display_list(root_background_color,
                                        Epoch(frame_number),
                                        self.window_size_f32(),
-                                       display_list,
+                                       display_list.finalize(),
                                        false);
 
         for (scroll_root_id, offset) in scroll_offsets {
