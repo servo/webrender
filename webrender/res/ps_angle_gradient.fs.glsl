e79d4448f562591e5c9f0d5d5047a0f4bf47efe9
Doug Thayer <dougathayer@gmail.com>
2017-03-14 23:18:26 -0700

    Added radial gradient benchmark

--- webrender/res/ps_angle_gradient.fs.glsl
+++ webrender/res/ps_angle_gradient.fs.glsl
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-uniform usampler2D sGradients;
+uniform highp usampler2D sGradients;
 
 void main(void) {
     vec2 texture_size = vec2(textureSize(sGradients, 0));
@@ -16,8 +16,8 @@ void main(void) {
     int x0 = int(2.0 * floor(x));
     int x1 = x0 + 1;
 
-    uvec4 color0 = texelFetch(sGradients, ivec2(x0, vGradientIndex), 0);
-    uvec4 color1 = texelFetch(sGradients, ivec2(x1, vGradientIndex), 0);
+    vec4 color0 = vec4(texelFetch(sGradients, ivec2(x0, vGradientIndex), 0));
+    vec4 color1 = vec4(texelFetch(sGradients, ivec2(x1, vGradientIndex), 0));
 
     vec4 color = mix(color0, color1, fract(x));
 
