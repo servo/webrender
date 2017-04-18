e83ff767a93bc6394d2f8dccabdd8b3df64e9a1b
alfredoyang <ayang@mozilla.com>
2017-04-18 17:38:38 +0800

    Support serialization to NativeFontHandle

--- webrender_traits/src/lib.rs
+++ webrender_traits/src/lib.rs
@@ -22,6 +22,9 @@ extern crate serde;
 extern crate serde_derive;
 
 #[cfg(target_os = "macos")]
+extern crate core_foundation;
+
+#[cfg(target_os = "macos")]
 extern crate core_graphics;
 
 #[cfg(target_os = "windows")]
