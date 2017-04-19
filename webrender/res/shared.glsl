141405a7309970d2efe0ce8f2c119a1e1f721b17
JerryShih <hshih@mozilla.com>
2017-04-19 22:14:27 +0800

    Add samplerExternalOES support.

--- webrender/res/shared.glsl
+++ webrender/res/shared.glsl
@@ -2,6 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifdef WR_FEATURE_TEXTURE_EXTERNAL
+// Please check https://www.khronos.org/registry/OpenGL/extensions/OES/OES_EGL_image_external_essl3.txt
+// for this extension.
+#extension GL_OES_EGL_image_external_essl3 : require
+#endif
+
 //======================================================================================
 // Vertex shader attributes and uniforms
 //======================================================================================
@@ -33,15 +39,20 @@
 //======================================================================================
 // Shared shader uniforms
 //======================================================================================
-#ifndef WR_FEATURE_TEXTURE_RECT
-uniform sampler2D sColor0;
-uniform sampler2D sColor1;
-uniform sampler2D sColor2;
-#else
+#ifdef WR_FEATURE_TEXTURE_RECT
 uniform sampler2DRect sColor0;
 uniform sampler2DRect sColor1;
 uniform sampler2DRect sColor2;
+#elif defined WR_FEATURE_TEXTURE_EXTERNAL
+uniform samplerExternalOES sColor0;
+uniform samplerExternalOES sColor1;
+uniform samplerExternalOES sColor2;
+#else
+uniform sampler2D sColor0;
+uniform sampler2D sColor1;
+uniform sampler2D sColor2;
 #endif
+
 uniform sampler2D sDither;
 
 //======================================================================================
