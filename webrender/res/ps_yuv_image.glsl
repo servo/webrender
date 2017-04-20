6e30b10da4f99072dde50e5f2142c440bd995bac
JerryShih <hshih@mozilla.com>
2017-04-20 15:05:06 +0800

    Add samplerExternalOES, sampler2dRect target and NV12 format support in ps_yuv_image.
    Use WR_FEATURE_TEXTURE_EXTERNAL for samplerExternalOES support.
    Use WR_FEATURE_TEXTURE_RECT for sampler2dRect support.
    Use WR_FEATURE_NV12 for NV12 format support.
    The WR_FEATURE_TEXTURE_EXTERNAL and WR_FEATURE_TEXTURE_RECT are exclusive.

--- webrender/res/ps_yuv_image.glsl
+++ webrender/res/ps_yuv_image.glsl
@@ -2,6 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// If this is in WR_FEATURE_TEXTURE_RECT mode, the rect and size use non-normalized
+// texture coordinates. Otherwise, it uses normalized texture coordinates. Please
+// check GL_TEXTURE_RECTANGLE.
 flat varying vec2 vTextureOffsetY; // Offset of the y plane into the texture atlas.
 flat varying vec2 vTextureOffsetU; // Offset of the u plane into the texture atlas.
 flat varying vec2 vTextureOffsetV; // Offset of the v plane into the texture atlas.
--- webrender/res/ps_yuv_image.vs.glsl
+++ webrender/res/ps_yuv_image.vs.glsl
@@ -28,33 +28,45 @@ void main(void) {
 
     ResourceRect y_rect = fetch_resource_rect(prim.user_data.x);
     ResourceRect u_rect = fetch_resource_rect(prim.user_data.x + 1);
+#ifndef WR_FEATURE_NV12
     ResourceRect v_rect = fetch_resource_rect(prim.user_data.x + 2);
+#endif
 
-    vec2 y_texture_size = vec2(textureSize(sColor0, 0));
-    vec2 y_st0 = y_rect.uv_rect.xy / y_texture_size;
-    vec2 y_st1 = y_rect.uv_rect.zw / y_texture_size;
+    // If this is in WR_FEATURE_TEXTURE_RECT mode, the rect and size use
+    // non-normalized texture coordinates.
+#ifdef WR_FEATURE_TEXTURE_RECT
+    vec2 y_texture_size_normalization_factor = vec2(1, 1);
+#else
+    vec2 y_texture_size_normalization_factor = vec2(textureSize(sColor0, 0));
+#endif
+    vec2 y_st0 = y_rect.uv_rect.xy / y_texture_size_normalization_factor;
+    vec2 y_st1 = y_rect.uv_rect.zw / y_texture_size_normalization_factor;
 
     vTextureSizeY = y_st1 - y_st0;
     vTextureOffsetY = y_st0;
 
-    vec2 uv_texture_size = vec2(textureSize(sColor1, 0));
-    vec2 u_st0 = u_rect.uv_rect.xy / uv_texture_size;
-    vec2 u_st1 = u_rect.uv_rect.zw / uv_texture_size;
-
-    vec2 v_st0 = v_rect.uv_rect.xy / uv_texture_size;
-    vec2 v_st1 = v_rect.uv_rect.zw / uv_texture_size;
-
     // This assumes the U and V surfaces have the same size.
+#ifdef WR_FEATURE_TEXTURE_RECT
+    vec2 uv_texture_size_normalization_factor = vec2(1, 1);
+#else
+    vec2 uv_texture_size_normalization_factor = vec2(textureSize(sColor1, 0));
+#endif
+    vec2 u_st0 = u_rect.uv_rect.xy / uv_texture_size_normalization_factor;
+    vec2 u_st1 = u_rect.uv_rect.zw / uv_texture_size_normalization_factor;
+
+#ifndef WR_FEATURE_NV12
+    vec2 v_st0 = v_rect.uv_rect.xy / uv_texture_size_normalization_factor;
+#endif
+
     vTextureSizeUv = u_st1 - u_st0;
     vTextureOffsetU = u_st0;
+#ifndef WR_FEATURE_NV12
     vTextureOffsetV = v_st0;
+#endif
 
     YuvImage image = fetch_yuv_image(prim.prim_index);
     vStretchSize = image.size;
 
-    vHalfTexelY = vec2(0.5) / y_texture_size;
-    vHalfTexelUv = vec2(0.5) / uv_texture_size;
-
-
-
+    vHalfTexelY = vec2(0.5) / y_texture_size_normalization_factor;
+    vHalfTexelUv = vec2(0.5) / uv_texture_size_normalization_factor;
 }
