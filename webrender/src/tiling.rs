18534562c006ca67c9682354147fd131cee85d08
JerryShih <hshih@mozilla.com>
2017-09-05 15:02:19 +0800

    Skip the request and return a invalid CacheItem for the over-size-limit image request.
    WR can't handle the image request if it's bigger than the hardware limit. Please check texture_cache::max_texture_size().
    
    Skip the image request for a big size image in resource_cache::request_image().
    Return an invalid CacheItem for a big size iin resource_cache::get_cached_image().

--- webrender/src/tiling.rs
+++ webrender/src/tiling.rs
@@ -458,6 +458,7 @@ impl AlphaRenderItem {
                         );
 
                         if color_texture_id == SourceTexture::Invalid {
+                            warn!("Warnings: skip a PrimitiveKind::Image at {:?}.\n", item_bounding_rect);
                             return;
                         }
 
@@ -516,6 +517,8 @@ impl AlphaRenderItem {
                             glyph_fetch_buffer,
                             gpu_cache,
                             |texture_id, glyphs| {
+                                debug_assert!(texture_id != SourceTexture::Invalid);
+
                                 let textures = BatchTextures {
                                     colors: [
                                         texture_id,
@@ -609,6 +612,7 @@ impl AlphaRenderItem {
                             );
 
                             if texture == SourceTexture::Invalid {
+                                warn!("Warnings: skip a PrimitiveKind::YuvImage at {:?}.\n", item_bounding_rect);
                                 return;
                             }
 
@@ -813,6 +817,11 @@ impl ClipBatcher {
                     ClipSource::Image(ref mask) => {
                         let cache_item =
                             resource_cache.get_cached_image(mask.image, ImageRendering::Auto, None);
+                        // Skip the invalid image mask.
+                        if cache_item.texture_id == SourceTexture::Invalid {
+                            warn!("Warnings: skip a image mask. Key:{:?} Rect::{:?}.\n", mask.image, mask.rect);
+                            continue;
+                        }
                         self.images
                             .entry(cache_item.texture_id)
                             .or_insert(Vec::new())
