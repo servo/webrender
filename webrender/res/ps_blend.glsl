82cc7df75b6c631b9ef54700b844df3c0729fd0b
Ethan Lin <ethlin@mozilla.com>
2017-11-16 14:13:12 +0800

    Make sure we un-premultiply alpha before applying filters.

--- webrender/res/ps_blend.glsl
+++ webrender/res/ps_blend.glsl
@@ -91,33 +91,22 @@ void main(void) {
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 vec4 Contrast(vec4 Cs, float amount) {
-    return vec4(Cs.rgb * amount - 0.5 * amount + 0.5, 1.0);
+    return vec4(Cs.rgb * amount - 0.5 * amount + 0.5, Cs.a);
 }
 
 vec4 Invert(vec4 Cs, float amount) {
-    Cs.rgb /= Cs.a;
-
-    vec3 color = mix(Cs.rgb, vec3(1.0) - Cs.rgb, amount);
-
-    // Pre-multiply the alpha into the output value.
-    return vec4(color.rgb * Cs.a, Cs.a);
+    return vec4(mix(Cs.rgb, vec3(1.0) - Cs.rgb, amount), Cs.a);
 }
 
 vec4 Brightness(vec4 Cs, float amount) {
-    // Un-premultiply the input.
-    Cs.rgb /= Cs.a;
-
     // Apply the brightness factor.
     // Resulting color needs to be clamped to output range
     // since we are pre-multiplying alpha in the shader.
-    vec3 color = clamp(Cs.rgb * amount, vec3(0.0), vec3(1.0));
-
-    // Pre-multiply the alpha into the output value.
-    return vec4(color.rgb * Cs.a, Cs.a);
+    return vec4(clamp(Cs.rgb * amount, vec3(0.0), vec3(1.0)), Cs.a);
 }
 
 vec4 Opacity(vec4 Cs, float amount) {
-    return Cs * amount;
+    return vec4(Cs.rgb, Cs.a * amount);
 }
 
 void main(void) {
@@ -127,6 +116,9 @@ void main(void) {
     if (Cs.a == 0.0) {
         discard;
     }
+
+    // Un-premultiply the input.
+    Cs.rgb /= Cs.a;
 
     switch (vOp) {
         case 0:
@@ -147,5 +139,8 @@ void main(void) {
         default:
             oFragColor = vColorMat * Cs;
     }
+
+    // Pre-multiply the alpha into the output value.
+    oFragColor.rgb *= oFragColor.a;
 }
 #endif
