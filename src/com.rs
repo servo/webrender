307b3f0dab64074b31c433a2006ed88e82b436b7
Simon Sapin <simon.sapin@exyr.org>
2018-02-13 16:33:19 +0100

    Add a COM pointer abstraction

---
+++ src/com.rs
@@ -0,0 +1,96 @@
+//! Similar to https://github.com/retep998/wio-rs/blob/44093f7db8/src/com.rs , but can be null
+
+use std::ops::Deref;
+use std::ptr::null_mut;
+use winapi;
+use winapi::Interface;
+use winapi::ctypes::c_void;
+use winapi::shared::winerror::HRESULT;
+use winapi::shared::winerror::SUCCEEDED;
+use winapi::um::unknwnbase::IUnknown;
+
+pub trait ToResult: Sized {
+    fn to_result(self) -> Result<(), Self>;
+}
+
+impl ToResult for HRESULT {
+    fn to_result(self) -> Result<(), Self> {
+        if SUCCEEDED(self) {
+            Ok(())
+        } else {
+            Err(self)
+        }
+    }
+}
+
+/// Nullable pointer to an owned COM object
+pub struct Com<T> where T: AsIUnknown {
+    ptr: *mut T,
+}
+
+impl<T> Com<T> where T: AsIUnknown {
+    pub fn null() -> Self {
+        Com { ptr: null_mut() }
+    }
+
+    pub fn as_ref(&self) -> Option<&T> {
+        unsafe {
+            self.ptr.as_ref()
+        }
+    }
+
+    pub unsafe fn as_ptr_ptr(&mut self) -> *mut *mut T {
+        &mut self.ptr
+    }
+
+    pub unsafe fn as_void_ptr_ptr(&mut self) -> *mut *mut c_void {
+        self.as_ptr_ptr() as _
+    }
+
+    pub fn query_interface<U>(&self) -> Result<Com<U>, HRESULT> where U: AsIUnknown {
+        let mut obj = Com::<U>::null();
+        unsafe {
+            self.as_iunknown().QueryInterface(&U::uuidof(), obj.as_void_ptr_ptr()).to_result()?
+        };
+        Ok(obj)
+    }
+}
+
+impl<T> Deref for Com<T> where T: AsIUnknown {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        self.as_ref().expect("dereferencing a null COM pointer")
+    }
+}
+
+impl<T> Drop for Com<T> where T: AsIUnknown {
+    fn drop(&mut self) {
+        unsafe {
+            if let Some(r) = self.ptr.as_ref() {
+                r.as_iunknown().Release();
+            }
+        }
+    }
+}
+
+pub trait AsIUnknown: Interface {
+    fn as_iunknown(&self) -> &IUnknown;
+}
+
+macro_rules! impl_as_iunknown {
+    ($( $ty: path, )+) => {
+        $(
+            impl AsIUnknown for $ty {
+                fn as_iunknown(&self) -> &IUnknown { self }
+            }
+        )+
+    }
+}
+
+impl_as_iunknown! {
+    winapi::shared::dxgi::IDXGIDevice,
+    winapi::um::d3d11::ID3D11Device,
+    winapi::um::dcomp::IDCompositionDevice,
+    winapi::um::dcomp::IDCompositionTarget,
+}
--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,14 @@ extern crate gl;
 extern crate glutin;
 extern crate winapi;
 
+use com::{Com, ToResult};
 use glutin::GlContext;
 use glutin::os::windows::WindowExt;
-use std::ops::Deref;
-use std::os::raw::c_void;
 use std::ptr;
 use winapi::Interface;
 use winapi::shared::dxgi::IDXGIDevice;
 use winapi::shared::minwindef::TRUE;
 use winapi::shared::winerror::HRESULT;
-use winapi::shared::winerror::SUCCEEDED;
 use winapi::shared::windef::HWND;
 use winapi::um::d3d11::D3D11_CREATE_DEVICE_BGRA_SUPPORT;
 use winapi::um::d3d11::D3D11_SDK_VERSION;
@@ -23,7 +21,8 @@ use winapi::um::d3dcommon::D3D_DRIVER_TYPE_HARDWARE;
 use winapi::um::dcomp::IDCompositionDevice;
 use winapi::um::dcomp::IDCompositionTarget;
 use winapi::um::dcomp::DCompositionCreateDevice;
-use winapi::um::unknwnbase::IUnknown;
+
+mod com;
 
 fn main() {
     let mut events_loop = glutin::EventsLoop::new();
@@ -68,38 +67,12 @@ fn main() {
     println!("Ok")
 }
 
-trait ToResult: Sized {
-    fn to_result(self) -> Result<(), Self>;
-}
-
-impl ToResult for HRESULT {
-    fn to_result(self) -> Result<(), Self> {
-        if SUCCEEDED(self) {
-            Ok(())
-        } else {
-            Err(self)
-        }
-    }
-}
-
-fn as_void<T>(p: *mut *mut T) -> *mut *mut c_void {
-    p as _
-}
-
-unsafe fn query_interface<T, U>(p: *mut T) -> Result<*mut U, HRESULT>
-    where T: Deref<Target=IUnknown>, U: Interface
-{
-    let mut ret = ptr::null_mut();
-    (*p).QueryInterface(&U::uuidof(), as_void(&mut ret)).to_result()?;
-    Ok(ret)
-}
-
 /// https://msdn.microsoft.com/en-us/library/windows/desktop/hh449180(v=vs.85).aspx
 
-unsafe fn initialize_direct_composition_device(m_hwnd: HWND)
-    -> Result<(*mut ID3D11Device, *mut IDCompositionDevice, *mut IDCompositionTarget), HRESULT>
+unsafe fn initialize_direct_composition_device(hwnd: HWND)
+    -> Result<(Com<ID3D11Device>, Com<IDCompositionDevice>, Com<IDCompositionTarget>), HRESULT>
 {
-    let mut m_pD3D11Device = ptr::null_mut();
+    let mut d3d_device = Com::<ID3D11Device>::null();
     let mut featureLevelSupported = 0;
 
     // Create the D3D device object. The D3D11_CREATE_DEVICE_BGRA_SUPPORT
@@ -112,27 +85,30 @@ unsafe fn initialize_direct_composition_device(m_hwnd: HWND)
         ptr::null_mut(),
         0,
         D3D11_SDK_VERSION,
-        &mut m_pD3D11Device,
+        d3d_device.as_ptr_ptr(),
         &mut featureLevelSupported,
         ptr::null_mut(),
     ).to_result()?;
 
     // Create the DXGI device used to create bitmap surfaces.
-    let pDXGIDevice: *mut IDXGIDevice = query_interface(m_pD3D11Device)?;
+    let pDXGIDevice = d3d_device.query_interface::<IDXGIDevice>()?;
 
     // Create the DirectComposition device object.
-    let mut m_pDCompDevice: *mut IDCompositionDevice = ptr::null_mut();
+    let mut composition_device = Com::<IDCompositionDevice>::null();
     DCompositionCreateDevice(
-        pDXGIDevice, &IDCompositionDevice::uuidof(), as_void(&mut m_pDCompDevice),
+        &*pDXGIDevice,
+        &IDCompositionDevice::uuidof(),
+        composition_device.as_void_ptr_ptr(),
     ).to_result()?;
 
     // Create the composition target object based on the
     // specified application window.
-    let mut m_pDCompTarget = ptr::null_mut();
-    (*m_pDCompDevice).CreateTargetForHwnd(m_hwnd, TRUE, &mut m_pDCompTarget).to_result()?;
+    let mut composition_target = Com::<IDCompositionTarget>::null();
+    composition_device.CreateTargetForHwnd(
+        hwnd,
+        TRUE,
+        composition_target.as_ptr_ptr(),
+    ).to_result()?;
 
-    // FIXME: make/use a type with a destructor, so that this runs even in case of early return
-    (*pDXGIDevice).Release();
-
-    Ok((m_pD3D11Device, m_pDCompDevice, m_pDCompTarget))
+    Ok((d3d_device, composition_device, composition_target))
 }
