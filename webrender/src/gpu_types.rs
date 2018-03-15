b44d9601e1e432ec455438a8221cdcd3910a1d93
Glenn Watson <github@intuitionlibrary.com>
2018-03-15 13:17:02 +1000

    Fix artifacts on box shadows that don't use nine-patch stretching.
    Fixes #2467.

--- webrender/src/gpu_types.rs
+++ webrender/src/gpu_types.rs
@@ -18,6 +18,15 @@ pub enum RasterizationSpace {
     Screen = 1,
 }
 
+#[derive(Debug, Copy, Clone)]
+#[cfg_attr(feature = "capture", derive(Serialize))]
+#[cfg_attr(feature = "replay", derive(Deserialize))]
+#[repr(C)]
+pub enum BoxShadowStretchMode {
+    Stretch = 0,
+    Simple = 1,
+}
+
 #[repr(i32)]
 #[derive(Debug, Copy, Clone)]
 #[cfg_attr(feature = "capture", derive(Serialize))]
