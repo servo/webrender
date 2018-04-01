74d05cc2f5eec1b4025edb0f601bd82e0bf7e131
Jon Leighton <j@jonathanleighton.com>
2018-04-01 19:00:57 +1000

    Use serde_bytes crate
    This speeds up serializing and deserializing of binary data.

--- webrender_api/src/api.rs
+++ webrender_api/src/api.rs
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+extern crate serde_bytes;
+
 use app_units::Au;
 use channel::{self, MsgSender, Payload, PayloadSender, PayloadSenderHelperMethods};
 use std::cell::Cell;
@@ -405,7 +407,11 @@ pub struct UpdateImage {
 
 #[derive(Clone, Deserialize, Serialize)]
 pub enum AddFont {
-    Raw(FontKey, Vec<u8>, u32),
+    Raw(
+        FontKey,
+        #[serde(with = "serde_bytes")] Vec<u8>,
+        u32
+    ),
     Native(FontKey, NativeFontHandle),
 }
 
