34a69834abda6b781fd783741285bee50fdd994e
Glenn Watson <github@intuitionlibrary.com>
2018-04-04 16:14:15 +1000

    Apply device pixel snapping to axis-aligned clip masks.
    Fixes #2501.

--- webrender/res/prim_shared.glsl
+++ webrender/res/prim_shared.glsl
@@ -486,11 +486,8 @@ vec4 get_node_pos(vec2 pos, ClipScrollNode node) {
     return untransform(pos, n, a, node.inv_transform);
 }
 
-// Compute a snapping offset in world space (adjusted to pixel ratio),
-// given local position on the scroll_node and a snap rectangle.
-vec2 compute_snap_offset(vec2 local_pos,
-                         mat4 transform,
-                         RectWithSize snap_rect) {
+// Compute a snapping offset in world space (adjusted to pixel ratio)
+vec4 compute_snap_positions(mat4 transform, RectWithSize snap_rect) {
     // Ensure that the snap rect is at *least* one device pixel in size.
     // TODO(gw): It's not clear to me that this is "correct". Specifically,
     //           how should it interact with sub-pixel snap rects when there
@@ -505,13 +502,45 @@ vec2 compute_snap_offset(vec2 local_pos,
     // Snap bounds in world coordinates, adjusted for pixel ratio. XY = top left, ZW = bottom right
     vec4 world_snap = uDevicePixelRatio * vec4(world_snap_p0.xy, world_snap_p1.xy) /
                                           vec4(world_snap_p0.ww, world_snap_p1.ww);
+    return world_snap;
+}
+
+vec2 compute_snap_offset_impl(
+    vec2 reference_pos,
+    mat4 transform,
+    RectWithSize snap_rect,
+    RectWithSize reference_rect,
+    vec4 snap_positions) {
+
     /// World offsets applied to the corners of the snap rectangle.
-    vec4 snap_offsets = floor(world_snap + 0.5) - world_snap;
+    vec4 snap_offsets = floor(snap_positions + 0.5) - snap_positions;
 
     /// Compute the position of this vertex inside the snap rectangle.
-    vec2 normalized_snap_pos = (local_pos - snap_rect.p0) / snap_rect.size;
+    vec2 normalized_snap_pos = (reference_pos - reference_rect.p0) / reference_rect.size;
+
     /// Compute the actual world offset for this vertex needed to make it snap.
     return mix(snap_offsets.xy, snap_offsets.zw, normalized_snap_pos);
+}
+
+// Compute a snapping offset in world space (adjusted to pixel ratio),
+// given local position on the scroll_node and a snap rectangle.
+vec2 compute_snap_offset(vec2 local_pos,
+                         mat4 transform,
+                         RectWithSize snap_rect) {
+    vec4 snap_positions = compute_snap_positions(
+        transform,
+        snap_rect
+    );
+
+    vec2 snap_offsets = compute_snap_offset_impl(
+        local_pos,
+        transform,
+        snap_rect,
+        snap_rect,
+        snap_positions
+    );
+
+    return snap_offsets;
 }
 
 struct VertexInfo {
