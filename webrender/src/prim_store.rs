bd5ad12d377a22173ee5948163aed466a7fb5439
Nicolas Silva <nical@fastmail.com>
2018-04-05 18:45:00 +0200

    Implement repetition in the brush image shader.

--- webrender/src/prim_store.rs
+++ webrender/src/prim_store.rs
@@ -2243,8 +2243,8 @@ impl<'a> GpuDataRequest<'a> {
     ) {
         self.push(local_rect);
         self.push([
-            0.0,
-            0.0,
+            1.0,
+            1.0,
             0.0,
             0.0
         ]);
