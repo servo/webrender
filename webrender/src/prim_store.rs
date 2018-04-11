7e6bdafee333e48d48f313ca0cd2d07752e15cad
Nicolas Silva <nical@fastmail.com>
2018-04-11 16:35:11 +0200

    Repeat images in the brush shader when there is no spacing.

--- webrender/src/prim_store.rs
+++ webrender/src/prim_store.rs
@@ -1473,6 +1473,7 @@ impl PrimitiveStore {
                     brush.write_gpu_blocks(&mut request);
 
                     let repeat = match brush.kind {
+                        BrushKind::Image { stretch_size, .. } |
                         BrushKind::LinearGradient { stretch_size, .. } |
                         BrushKind::RadialGradient { stretch_size, .. } => {
                             [
