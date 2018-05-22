b7ce5206052f5677a303c3f88f86408619c83f33
Hugh Gallagher <hgallagher1993@gmail.com>
2018-05-22 21:13:23 +0100

    Fix compile error

--- wrench/src/wrench.rs
+++ wrench/src/wrench.rs
@@ -385,9 +385,9 @@ impl Wrench {
     #[cfg(target_os = "windows")]
     pub fn font_key_from_native_handle(&mut self, descriptor: &NativeFontHandle) -> FontKey {
         let key = self.api.generate_font_key();
-        let mut resources = ResourceUpdates::new();
-        resources.add_native_font(key, descriptor.clone());
-        self.api.update_resources(resources);
+        let mut txn = Transaction::new();
+        txn.add_native_font(key, descriptor.clone());
+        self.api.update_resources(txn.resource_updates);
         key
     }
 
