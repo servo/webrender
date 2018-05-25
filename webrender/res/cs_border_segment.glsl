5d6a4ff073063d7ab7897fe9d10fd4d834734bf4
Glenn Watson <github@intuitionlibrary.com>
2018-05-25 16:35:02 +1000

    Support double style borders in new border brush path.

--- webrender/res/cs_border_segment.glsl
+++ webrender/res/cs_border_segment.glsl
@@ -4,18 +4,57 @@
 
 #include shared,prim_shared,ellipse,shared_border
 
+// For edges, the colors are the same. For corners, these
+// are the colors of each edge making up the corner.
 flat varying vec4 vColor0;
 flat varying vec4 vColor1;
+
+// A point + tangent defining the line where the edge
+// transition occurs. Used for corners only.
 flat varying vec4 vColorLine;
-flat varying int vFeatures;
+
+// Pass through the border style directly. Style
+// is the same for edges, may be different for corners.
+flat varying ivec2 vStyle;
+
+// x = true if corner, y and z specify if edge is
+// horizontal or vertical.
+flat varying ivec3 vConfig;
+
+// Local space position of the clip center.
 flat varying vec2 vClipCenter;
+
+// An outer and inner elliptical radii for border
+// corner clipping.
 flat varying vec4 vClipRadii;
+
+// Scale the rect origin by this to get the outer
+// corner from the segment rectangle.
 flat varying vec2 vClipSign;
 
+// Widths of the edges for this edge or corner.
+flat varying vec2 vWidths;
+
+// Vertical / horizontal clip distances for double style.
+flat varying vec4 vEdgeClip;
+
+// Stores widths/2 and widths/3 to save doing this in FS.
+flat varying vec4 vPartialWidths;
+
+// Local space position
 varying vec2 vPos;
 
-#define CLIP_RADII      1
-#define MIX_COLOR       2
+#define SEGMENT_TOP_LEFT        0
+#define SEGMENT_TOP_RIGHT       1
+#define SEGMENT_BOTTOM_RIGHT    2
+#define SEGMENT_BOTTOM_LEFT     3
+#define SEGMENT_LEFT            4
+#define SEGMENT_TOP             5
+#define SEGMENT_RIGHT           6
+#define SEGMENT_BOTTOM          7
+
+#define EDGE_VERTICAL           0
+#define EDGE_HORIZONTAL         1
 
 #ifdef WR_VERTEX_SHADER
 
@@ -26,15 +65,6 @@ in vec4 aColor1;
 in int aFlags;
 in vec2 aWidths;
 in vec2 aRadii;
-
-#define SEGMENT_TOP_LEFT        0
-#define SEGMENT_TOP_RIGHT       1
-#define SEGMENT_BOTTOM_RIGHT    2
-#define SEGMENT_BOTTOM_LEFT     3
-#define SEGMENT_LEFT            4
-#define SEGMENT_TOP             5
-#define SEGMENT_RIGHT           6
-#define SEGMENT_BOTTOM          7
 
 vec2 get_outer_corner_scale(int segment) {
     vec2 p;
@@ -62,66 +92,200 @@ vec2 get_outer_corner_scale(int segment) {
 }
 
 void main(void) {
-    vec2 pos = aRect.xy + aRect.zw * aPosition.xy;
-
     int segment = aFlags & 0xff;
-    int style = (aFlags >> 8) & 0xff;
+    int style0 = (aFlags >> 8) & 0xff;
+    int style1 = (aFlags >> 16) & 0xff;
 
     vec2 outer_scale = get_outer_corner_scale(segment);
-    vec2 outer = aRect.xy + outer_scale * aRect.zw;
+    vec2 outer = outer_scale * aRect.zw;
     vec2 clip_sign = 1.0 - 2.0 * outer_scale;
 
-    vColor0 = aColor0;
-    vColor1 = aColor1;
-    vPos = pos;
-
-    vFeatures = 0;
-    vClipSign = clip_sign;
-    vClipCenter = outer + clip_sign * aRadii;
-    vClipRadii = vec4(aRadii, aRadii - aWidths);
-    vColorLine = vec4(0.0);
-
+    ivec3 cfg;
     switch (segment) {
         case SEGMENT_TOP_LEFT:
+            cfg = ivec3(true, EDGE_HORIZONTAL, EDGE_VERTICAL);
+            break;
         case SEGMENT_TOP_RIGHT:
+            cfg = ivec3(true, EDGE_VERTICAL, EDGE_HORIZONTAL);
+            break;
         case SEGMENT_BOTTOM_RIGHT:
+            cfg = ivec3(true, EDGE_HORIZONTAL, EDGE_VERTICAL);
+            break;
         case SEGMENT_BOTTOM_LEFT:
-            vFeatures |= (CLIP_RADII | MIX_COLOR);
-            vColorLine = vec4(outer, aWidths.y * -clip_sign.y, aWidths.x * clip_sign.x);
+            cfg = ivec3(true, EDGE_VERTICAL, EDGE_HORIZONTAL);
+            break;
+        case SEGMENT_LEFT:
+        case SEGMENT_RIGHT:
+            cfg = ivec3(false, EDGE_HORIZONTAL, EDGE_HORIZONTAL);
+            break;
+        case SEGMENT_TOP:
+        case SEGMENT_BOTTOM:
+            cfg = ivec3(false, EDGE_VERTICAL, EDGE_VERTICAL);
             break;
         default:
             break;
     }
 
-    gl_Position = uTransform * vec4(aTaskOrigin + pos, 0.0, 1.0);
+    vConfig = cfg;
+    vStyle = ivec2(style0, style1);
+    vColor0 = aColor0;
+    vColor1 = aColor1;
+    vWidths = aWidths;
+    vPartialWidths = vec4(vWidths / 3.0, vWidths / 2.0);
+    vPos = aRect.zw * aPosition.xy;
+
+    vClipSign = clip_sign;
+    vClipCenter = outer + clip_sign * aRadii;
+    vClipRadii = vec4(aRadii, max(aRadii - aWidths, 0.0));
+    vColorLine = vec4(outer, aWidths.y * -clip_sign.y, aWidths.x * clip_sign.x);
+
+    // Derive the positions for the edge clips, which must be handled
+    // differently between corners and edges.
+    if (cfg.x != 0) {
+        vec2 p0 = outer + clip_sign * vPartialWidths.xy;
+        vec2 p1 = outer + clip_sign * (vWidths - vPartialWidths.xy);
+
+        vEdgeClip = vec4(
+            min(p0, p1),
+            max(p0, p1)
+        );
+    } else {
+        vEdgeClip = vec4(vPartialWidths.xy, aRect.zw - vPartialWidths.xy);
+    }
+
+    gl_Position = uTransform * vec4(aTaskOrigin + aRect.xy + vPos, 0.0, 1.0);
 }
 #endif
 
 #ifdef WR_FRAGMENT_SHADER
+vec4 evaluate_color_for_style_in_corner(
+    vec2 clip_relative_pos,
+    int style,
+    vec4 color,
+    vec4 clip_radii,
+    vec2 widths,
+    float aa_range
+) {
+    switch (style) {
+        case BORDER_STYLE_DOUBLE: {
+            float d_radii_a = distance_to_ellipse(
+                clip_relative_pos,
+                clip_radii.xy - vPartialWidths.xy,
+                aa_range
+            );
+            float d_radii_b = distance_to_ellipse(
+                clip_relative_pos,
+                clip_radii.xy - 2.0 * vPartialWidths.xy,
+                aa_range
+            );
+            float d = min(-d_radii_a, d_radii_b);
+            float alpha = distance_aa(aa_range, d);
+            return alpha * color;
+        }
+        default:
+            break;
+    }
+
+    return color;
+}
+
+vec4 evaluate_color_for_style_in_edge(
+    vec2 pos,
+    int style,
+    vec4 color,
+    vec2 widths,
+    float aa_range,
+    int edge_kind
+) {
+    switch (style) {
+        case BORDER_STYLE_DOUBLE: {
+            float d0 = -1.0;
+            float d1 = -1.0;
+            if (edge_kind == EDGE_VERTICAL) {
+                // To pass reftests, we need to ensure that
+                // we don't apply edge clips for double style
+                // when there are < 3 device pixels.
+                if (widths.y > 3.0) {
+                    d0 = pos.y - vEdgeClip.y;
+                    d1 = vEdgeClip.w - pos.y;
+                }
+            } else {
+                if (widths.x > 3.0) {
+                    d0 = pos.x - vEdgeClip.x;
+                    d1 = vEdgeClip.z - pos.x;
+                }
+            }
+            float d = min(d0, d1);
+            float alpha = distance_aa(aa_range, d);
+            return alpha * color;
+        }
+        default:
+            break;
+    }
+
+    return color;
+}
+
 void main(void) {
     float aa_range = compute_aa_range(vPos);
     float d = -1.0;
+    vec4 color0, color1;
 
-    // Apply color mix
+    // Determine which side of the edge transition this
+    // fragment belongs to.
     float mix_factor = 0.0;
-    if ((vFeatures & MIX_COLOR) != 0) {
+    if (vConfig.x != 0) {
         float d_line = distance_to_line(vColorLine.xy, vColorLine.zw, vPos);
         mix_factor = distance_aa(aa_range, -d_line);
     }
 
-    // Apply clip radii
-    if ((vFeatures & CLIP_RADII) != 0) {
-        vec2 p = vPos - vClipCenter;
-        if (vClipSign.x * p.x < 0.0 && vClipSign.y * p.y < 0.0) {
-            float d_radii_a = distance_to_ellipse(p, vClipRadii.xy, aa_range);
-            float d_radii_b = distance_to_ellipse(p, vClipRadii.zw, aa_range);
-            float d_radii = max(d_radii_a, -d_radii_b);
-            d = max(d, d_radii);
-        }
+    // Check if inside corner clip-region
+    vec2 clip_relative_pos = vPos - vClipCenter;
+    bool in_clip_region = all(lessThan(vClipSign * clip_relative_pos, vec2(0.0)));
+
+    if (in_clip_region) {
+        float d_radii_a = distance_to_ellipse(clip_relative_pos, vClipRadii.xy, aa_range);
+        float d_radii_b = distance_to_ellipse(clip_relative_pos, vClipRadii.zw, aa_range);
+        float d_radii = max(d_radii_a, -d_radii_b);
+        d = max(d, d_radii);
+
+        color0 = evaluate_color_for_style_in_corner(
+            clip_relative_pos,
+            vStyle.x,
+            vColor0,
+            vClipRadii,
+            vWidths,
+            aa_range
+        );
+        color1 = evaluate_color_for_style_in_corner(
+            clip_relative_pos,
+            vStyle.y,
+            vColor1,
+            vClipRadii,
+            vWidths,
+            aa_range
+        );
+    } else {
+        color0 = evaluate_color_for_style_in_edge(
+            vPos,
+            vStyle.x,
+            vColor0,
+            vWidths,
+            aa_range,
+            vConfig.y
+        );
+        color1 = evaluate_color_for_style_in_edge(
+            vPos,
+            vStyle.y,
+            vColor1,
+            vWidths,
+            aa_range,
+            vConfig.z
+        );
     }
 
     float alpha = distance_aa(aa_range, d);
-    vec4 color = mix(vColor0, vColor1, mix_factor);
+    vec4 color = mix(color0, color1, mix_factor);
     oFragColor = color * alpha;
 }
 #endif
