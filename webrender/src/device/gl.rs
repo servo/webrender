70840b1aa8bd19a2523ef781d9162c5158950100
Zakor Gyula <gyula.zakor@h-lab.eu>
2018-06-22 10:41:10 +0200

    Move device in a folder

--- webrender/src/device/gl.rs
+++ webrender/src/device/gl.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use super::shader_source;
+use super::super::shader_source;
 use api::{ColorF, ImageFormat};
 use api::{DeviceIntPoint, DeviceIntRect, DeviceUintRect, DeviceUintSize};
 use api::TextureTarget;
