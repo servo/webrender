servo/webrender

commit 7b5a93d41f99788bb378880d2db0b44896a1b63b

Reduce per-instance size from 32 -> 16 bytes.
Add a new PrimitiveHeader concept, which contains all the common
information for a given primitive (such as scroll node, render
task, z value etc).

Instances contain an index to the primitive header, and read the
information from that. This allows the instance size to be
halved. There is a slight extra vertex shader cost, due to
an extra indirection. However, this was not apparent in any
profiling I did, since the vertex shader time typically makes
up a very small amount of overall time. Additionally, the
primitive header texels are likely to be in the texture cache
for each glyph that is fetched. A possible future improvement
is to remove that extra indirection by writing some of that
data, such as the render task address, directly into the primitive
header.

On sites such as nytimes.com or wikipedia.org, where there are
typically a large number of glyphs, this saves several hundred
kB per frame of data being sent to the GPU, which improves the
compositor / driver CPU time.

Also remove the local_clip_chains vector and texture. Now, we just
calculate this as part of the primitive header and include it
there. This can save significant amounts of texture upload on
pages that have a lot of clip chains or clip nodes, relative to
the visible primitive count.

In future, both the PrimitiveHeader and instance structures can
easily be further compressed again, but this can be done as a
follow up.

Finally, although this is a useful optimization by itself, it's
mostly prep work for some changes I have planned related to
how we store and upload scroll nodes. Hopefully these changes will
be both an optimization and make it easier to fix some of the
correctness issues we have with nested 3d transforms.

webrender/src/prim_store.rs
@@ -20,7 +20,7 @@ use frame_builder::PrimitiveRunContext;
 use glyph_rasterizer::{FontInstance, FontTransform, GlyphKey, FONT_SIZE_LIMIT};
 use gpu_cache::{GpuBlockData, GpuCache, GpuCacheAddress, GpuCacheHandle, GpuDataRequest,
                 ToGpuBlocks};
-use gpu_types::{BrushFlags, ClipChainRectIndex};
+use gpu_types::BrushFlags;
 use image::{for_each_tile, for_each_repetition};
 use picture::{PictureCompositeMode, PictureId, PicturePrimitive};
 #[cfg(debug_assertions)]
@@ -174,7 +174,11 @@ pub struct PrimitiveMetadata {
     //           storing them here.
     pub local_rect: LayoutRect,
     pub local_clip_rect: LayoutRect,
-    pub clip_chain_rect_index: ClipChainRectIndex,
+
+    // The current combined local clip for this primitive, from
+    // the primitive local clip above and the current clip chain.
+    pub combined_local_clip_rect: LayoutRect,
+
     pub is_backface_visible: bool,
     pub screen_rect: Option<ScreenRect>,
 
@@ -234,11 +238,15 @@ pub struct VisibleImageTile {
     pub tile_offset: TileOffset,
     pub handle: GpuCacheHandle,
     pub edge_flags: EdgeAaSegmentMask,
+    pub local_rect: LayoutRect,
+    pub local_clip_rect: LayoutRect,
 }
 
 #[derive(Debug)]
 pub struct VisibleGradientTile {
     pub handle: GpuCacheHandle,
+    pub local_rect: LayoutRect,
+    pub local_clip_rect: LayoutRect,
 }
 
 #[derive(Debug)]
@@ -1213,7 +1221,7 @@ impl PrimitiveStore {
             clip_task_id: None,
             local_rect: *local_rect,
             local_clip_rect: *local_clip_rect,
-            clip_chain_rect_index: ClipChainRectIndex(0),
+            combined_local_clip_rect: *local_clip_rect,
             is_backface_visible,
             screen_rect: None,
             tag,
@@ -1684,8 +1692,6 @@ impl PrimitiveStore {
 
                                                 let mut handle = GpuCacheHandle::new();
                                                 if let Some(mut request) = frame_state.gpu_cache.request(&mut handle) {
-                                                    request.push(*tile_rect);
-                                                    request.push(tight_clip_rect);
                                                     request.push(ColorF::new(1.0, 1.0, 1.0, opacity_binding.current).premultiplied());
                                                     request.push(PremultipliedColorF::WHITE);
                                                     request.push([tile_rect.size.width, tile_rect.size.height, 0.0, 0.0]);
@@ -1696,6 +1702,8 @@ impl PrimitiveStore {
                                                     tile_offset,
                                                     handle,
                                                     edge_flags: tile_flags & edge_flags,
+                                                    local_rect: *tile_rect,
+                                                    local_clip_rect: tight_clip_rect,
                                                 });
                                             }
                                         );
@@ -1789,9 +1797,7 @@ impl PrimitiveStore {
                                 prim_run_context,
                                 frame_context,
                                 frame_state,
-                                &mut |rect, clip_rect, mut request| {
-                                    request.push(*rect);
-                                    request.push(*clip_rect);
+                                &mut |rect, mut request| {
                                     request.push([
                                         center.x,
                                         center.y,
@@ -1841,9 +1847,7 @@ impl PrimitiveStore {
                                 prim_run_context,
                                 frame_context,
                                 frame_state,
-                                &mut |rect, clip_rect, mut request| {
-                                    request.push(*rect);
-                                    request.push(*clip_rect);
+                                &mut |rect, mut request| {
                                     request.push([
                                         start_point.x,
                                         start_point.y,
@@ -1895,10 +1899,6 @@ impl PrimitiveStore {
 
         // Mark this GPU resource as required for this frame.
         if let Some(mut request) = frame_state.gpu_cache.request(&mut metadata.gpu_location) {
-            // has to match VECS_PER_BRUSH_PRIM
-            request.push(metadata.local_rect);
-            request.push(metadata.local_clip_rect);
-
             match metadata.prim_kind {
                 PrimitiveKind::TextRun => {
                     let text = &self.cpu_text_runs[metadata.cpu_prim_index.0];
@@ -2457,7 +2457,11 @@ impl PrimitiveStore {
                 clipped,
                 unclipped,
             });
-            metadata.clip_chain_rect_index = prim_run_context.clip_chain_rect_index;
+
+            metadata.combined_local_clip_rect = prim_run_context
+                .local_clip_rect
+                .intersection(&metadata.local_clip_rect)
+                .unwrap_or(LayoutRect::zero());
 
             (local_rect, unclipped)
         };
@@ -2565,19 +2569,16 @@ impl PrimitiveStore {
                 None
             };
 
-            let clip_chain_rect_index = match clip_chain_rect {
+            let local_clip_chain_rect = match clip_chain_rect {
                 Some(rect) if rect.is_empty() => continue,
-                Some(rect) => {
-                    frame_state.local_clip_rects.push(rect);
-                    ClipChainRectIndex(frame_state.local_clip_rects.len() - 1)
-                }
-                None => ClipChainRectIndex(0), // This is no clipping.
+                Some(rect) => rect,
+                None => frame_context.max_local_clip,
             };
 
             let child_prim_run_context = PrimitiveRunContext::new(
                 clip_chain,
                 scroll_node,
-                clip_chain_rect_index,
+                local_clip_chain_rect,
             );
 
             for i in 0 .. run.count {
@@ -2646,7 +2647,7 @@ fn decompose_repeated_primitive(
     prim_run_context: &PrimitiveRunContext,
     frame_context: &FrameBuildingContext,
     frame_state: &mut FrameBuildingState,
-    callback: &mut FnMut(&LayoutRect, &LayoutRect, GpuDataRequest),
+    callback: &mut FnMut(&LayoutRect, GpuDataRequest),
 ) {
     visible_tiles.clear();
 
@@ -2669,16 +2670,19 @@ fn decompose_repeated_primitive(
         &mut |origin, _| {
 
             let mut handle = GpuCacheHandle::new();
+            let rect = LayoutRect {
+                origin: *origin,
+                size: *stretch_size,
+            };
             if let Some(request) = frame_state.gpu_cache.request(&mut handle) {
-                let rect = LayoutRect {
-                    origin: *origin,
-                    size: *stretch_size,
-                };
-
-                callback(&rect, &tight_clip_rect, request);
+                callback(&rect, request);
             }
 
-            visible_tiles.push(VisibleGradientTile { handle });
+            visible_tiles.push(VisibleGradientTile {
+                local_rect: rect,
+                local_clip_rect: tight_clip_rect,
+                handle
+            });
         }
     );
 

webrender/res/brush.glsl
@@ -16,7 +16,6 @@ void brush_vs(
     vec4 segment_data
 );
 
-#define VECS_PER_BRUSH_PRIM                 2
 #define VECS_PER_SEGMENT                    2
 
 #define BRUSH_FLAG_PERSPECTIVE_INTERPOLATION    1
@@ -24,76 +23,19 @@ void brush_vs(
 #define BRUSH_FLAG_SEGMENT_REPEAT_X             4
 #define BRUSH_FLAG_SEGMENT_REPEAT_Y             8
 
-//Note: these have to match `gpu_types` constants
-#define INT_BITS    (31)
-#define CLIP_CHAIN_RECT_BITS    (22)
-#define SEGMENT_BITS (INT_BITS - CLIP_CHAIN_RECT_BITS)
-#define EDGE_FLAG_BITS (4)
-#define BRUSH_FLAG_BITS (4)
-#define CLIP_SCROLL_INDEX_BITS (INT_BITS - EDGE_FLAG_BITS - BRUSH_FLAG_BITS)
-
-struct BrushInstance {
-    int picture_address;
-    int prim_address;
-    int clip_chain_rect_index;
-    int scroll_node_id;
-    int clip_address;
-    int z;
-    int segment_index;
-    int edge_mask;
-    int flags;
-    ivec3 user_data;
-};
-
-BrushInstance load_brush() {
-    BrushInstance bi;
-
-    bi.picture_address = aData0.x & 0xffff;
-    bi.clip_address = aData0.x >> 16;
-    bi.prim_address = aData0.y;
-    bi.clip_chain_rect_index = aData0.z  & ((1 << CLIP_CHAIN_RECT_BITS) - 1);
-    bi.segment_index = aData0.z >> CLIP_CHAIN_RECT_BITS;
-    bi.z = aData0.w;
-    bi.scroll_node_id = aData1.x & ((1 << CLIP_SCROLL_INDEX_BITS) - 1);
-    bi.edge_mask = (aData1.x >> CLIP_SCROLL_INDEX_BITS) & 0xf;
-    bi.flags = (aData1.x >> (CLIP_SCROLL_INDEX_BITS + EDGE_FLAG_BITS)) & 0xf;
-    bi.user_data = aData1.yzw;
-
-    return bi;
-}
-
-struct BrushPrimitive {
-    RectWithSize local_rect;
-    RectWithSize local_clip_rect;
-};
-
-BrushPrimitive fetch_brush_primitive(int address, int clip_chain_rect_index) {
-    vec4 data[2] = fetch_from_resource_cache_2(address);
-
-    RectWithSize clip_chain_rect = fetch_clip_chain_rect(clip_chain_rect_index);
-    RectWithSize brush_clip_rect = RectWithSize(data[1].xy, data[1].zw);
-    RectWithSize clip_rect = intersect_rects(clip_chain_rect, brush_clip_rect);
-
-    BrushPrimitive prim = BrushPrimitive(RectWithSize(data[0].xy, data[0].zw), clip_rect);
-
-    return prim;
-}
-
 void main(void) {
     // Load the brush instance from vertex attributes.
-    BrushInstance brush = load_brush();
-
-    // Load the geometry for this brush. For now, this is simply the
-    // local rect of the primitive. In the future, this will support
-    // loading segment rects, and other rect formats (glyphs).
-    BrushPrimitive brush_prim =
-        fetch_brush_primitive(brush.prim_address, brush.clip_chain_rect_index);
+    int prim_header_address = aData.x;
+    int clip_address = aData.y;
+    int segment_index = aData.z & 0xffff;
+    int edge_flags = (aData.z >> 16) & 0xff;
+    int brush_flags = (aData.z >> 24) & 0xff;
+    PrimitiveHeader ph = fetch_prim_header(prim_header_address);
 
     // Fetch the segment of this brush primitive we are drawing.
-    int segment_address = brush.prim_address +
-                          VECS_PER_BRUSH_PRIM +
+    int segment_address = ph.specific_prim_address +
                           VECS_PER_SPECIFIC_BRUSH +
-                          brush.segment_index * VECS_PER_SEGMENT;
+                          segment_index * VECS_PER_SEGMENT;
 
     vec4[2] segment_data = fetch_from_resource_cache_2(segment_address);
     RectWithSize local_segment_rect = RectWithSize(segment_data[0].xy, segment_data[0].zw);
@@ -101,20 +43,20 @@ void main(void) {
     VertexInfo vi;
 
     // Fetch the dynamic picture that we are drawing on.
-    PictureTask pic_task = fetch_picture_task(brush.picture_address);
-    ClipArea clip_area = fetch_clip_area(brush.clip_address);
+    PictureTask pic_task = fetch_picture_task(ph.render_task_index);
+    ClipArea clip_area = fetch_clip_area(clip_address);
 
-    ClipScrollNode scroll_node = fetch_clip_scroll_node(brush.scroll_node_id);
+    ClipScrollNode scroll_node = fetch_clip_scroll_node(ph.scroll_node_id);
 
     // Write the normal vertex information out.
     if (scroll_node.is_axis_aligned) {
         vi = write_vertex(
             local_segment_rect,
-            brush_prim.local_clip_rect,
-            float(brush.z),
+            ph.local_clip_rect,
+            ph.z,
             scroll_node,
             pic_task,
-            brush_prim.local_rect
+            ph.local_rect
         );
 
         // TODO(gw): transform bounds may be referenced by
@@ -127,15 +69,15 @@ void main(void) {
         init_transform_vs(vec4(vec2(-1000000.0), vec2(1000000.0)));
 #endif
     } else {
-        bvec4 edge_mask = notEqual(brush.edge_mask & ivec4(1, 2, 4, 8), ivec4(0));
-        bool do_perspective_interpolation = (brush.flags & BRUSH_FLAG_PERSPECTIVE_INTERPOLATION) != 0;
+        bvec4 edge_mask = notEqual(edge_flags & ivec4(1, 2, 4, 8), ivec4(0));
+        bool do_perspective_interpolation = (brush_flags & BRUSH_FLAG_PERSPECTIVE_INTERPOLATION) != 0;
 
         vi = write_transform_vertex(
             local_segment_rect,
-            brush_prim.local_rect,
-            brush_prim.local_clip_rect,
+            ph.local_rect,
+            ph.local_clip_rect,
             mix(vec4(0.0), vec4(1.0), edge_mask),
-            float(brush.z),
+            ph.z,
             scroll_node,
             pic_task,
             do_perspective_interpolation
@@ -158,13 +100,13 @@ void main(void) {
     // Run the specific brush VS code to write interpolators.
     brush_vs(
         vi,
-        brush.prim_address + VECS_PER_BRUSH_PRIM,
-        brush_prim.local_rect,
+        ph.specific_prim_address,
+        ph.local_rect,
         local_segment_rect,
-        brush.user_data,
+        ph.user_data,
         scroll_node.transform,
         pic_task,
-        brush.flags,
+        brush_flags,
         segment_data[1]
     );
 }

webrender/res/prim_shared.glsl
@@ -32,11 +32,6 @@ varying vec3 vClipMaskUv;
 
 #ifdef WR_VERTEX_SHADER
 
-#define VECS_PER_LOCAL_CLIP_RECT    1
-#define VECS_PER_PRIM_HEADER        2
-#define VECS_PER_TEXT_RUN           3
-#define VECS_PER_GRADIENT_STOP      2
-
 #define COLOR_MODE_FROM_PASS          0
 #define COLOR_MODE_ALPHA              1
 #define COLOR_MODE_SUBPX_CONST_COLOR  2
@@ -47,16 +42,46 @@ varying vec3 vClipMaskUv;
 #define COLOR_MODE_BITMAP             7
 #define COLOR_MODE_COLOR_BITMAP       8
 
-uniform HIGHP_SAMPLER_FLOAT sampler2D sLocalClipRects;
+uniform HIGHP_SAMPLER_FLOAT sampler2D sPrimitiveHeadersF;
+uniform HIGHP_SAMPLER_FLOAT isampler2D sPrimitiveHeadersI;
 
 // Instanced attributes
-in ivec4 aData0;
-in ivec4 aData1;
+in ivec4 aData;
 
-RectWithSize fetch_clip_chain_rect(int index) {
-    ivec2 uv = get_fetch_uv(index, VECS_PER_LOCAL_CLIP_RECT);
-    vec4 rect = TEXEL_FETCH(sLocalClipRects, uv, 0, ivec2(0, 0));
-    return RectWithSize(rect.xy, rect.zw);
+#define VECS_PER_PRIM_HEADER_F 2
+#define VECS_PER_PRIM_HEADER_I 2
+
+struct PrimitiveHeader {
+    RectWithSize local_rect;
+    RectWithSize local_clip_rect;
+    float z;
+    int specific_prim_address;
+    int render_task_index;
+    int clip_task_index;
+    int scroll_node_id;
+    ivec3 user_data;
+};
+
+PrimitiveHeader fetch_prim_header(int index) {
+    PrimitiveHeader ph;
+
+    ivec2 uv_f = get_fetch_uv(index, VECS_PER_PRIM_HEADER_F);
+    vec4 local_rect = TEXEL_FETCH(sPrimitiveHeadersF, uv_f, 0, ivec2(0, 0));
+    vec4 local_clip_rect = TEXEL_FETCH(sPrimitiveHeadersF, uv_f, 0, ivec2(1, 0));
+    ph.local_rect = RectWithSize(local_rect.xy, local_rect.zw);
+    ph.local_clip_rect = RectWithSize(local_clip_rect.xy, local_clip_rect.zw);
+
+    ivec2 uv_i = get_fetch_uv(index, VECS_PER_PRIM_HEADER_I);
+    ivec4 data0 = TEXEL_FETCH(sPrimitiveHeadersI, uv_i, 0, ivec2(0, 0));
+    ivec4 data1 = TEXEL_FETCH(sPrimitiveHeadersI, uv_i, 0, ivec2(1, 0));
+    ph.z = float(data0.x);
+    ph.render_task_index = data0.y;
+    ph.specific_prim_address = data0.z;
+    ph.clip_task_index = data0.w;
+    ph.scroll_node_id = data1.x;
+    ph.user_data = data1.yzw;
+
+    return ph;
 }
 
 struct VertexInfo {

webrender/res/ps_split_composite.glsl
@@ -44,10 +44,10 @@ struct SplitCompositeInstance {
 SplitCompositeInstance fetch_composite_instance() {
     SplitCompositeInstance ci;
 
-    ci.render_task_index = aData0.x;
-    ci.src_task_index = aData0.y;
-    ci.polygons_address = aData0.z;
-    ci.z = float(aData0.w);
+    ci.render_task_index = aData.x;
+    ci.src_task_index = aData.y;
+    ci.polygons_address = aData.z;
+    ci.z = float(aData.w);
 
     return ci;
 }

webrender/res/ps_text_run.glsl
@@ -15,6 +15,8 @@ varying vec4 vUvClip;
 
 #ifdef WR_VERTEX_SHADER
 
+#define VECS_PER_TEXT_RUN           3
+
 struct Glyph {
     vec2 offset;
 };
@@ -55,84 +57,6 @@ struct TextRun {
 TextRun fetch_text_run(int address) {
     vec4 data[3] = fetch_from_resource_cache_3(address);
     return TextRun(data[0], data[1], data[2].xy);
-}
-
-struct PrimitiveInstance {
-    int prim_address;
-    int specific_prim_address;
-    int render_task_index;
-    int clip_task_index;
-    int scroll_node_id;
-    int clip_chain_rect_index;
-    int z;
-    int user_data0;
-    int user_data1;
-    int user_data2;
-};
-
-PrimitiveInstance fetch_prim_instance() {
-    PrimitiveInstance pi;
-
-    pi.prim_address = aData0.x;
-    pi.specific_prim_address = pi.prim_address + VECS_PER_PRIM_HEADER;
-    pi.render_task_index = aData0.y % 0x10000;
-    pi.clip_task_index = aData0.y / 0x10000;
-    pi.clip_chain_rect_index = aData0.z;
-    pi.scroll_node_id = aData0.w;
-    pi.z = aData1.x;
-    pi.user_data0 = aData1.y;
-    pi.user_data1 = aData1.z;
-    pi.user_data2 = aData1.w;
-
-    return pi;
-}
-
-struct Primitive {
-    ClipScrollNode scroll_node;
-    ClipArea clip_area;
-    PictureTask task;
-    RectWithSize local_rect;
-    RectWithSize local_clip_rect;
-    int specific_prim_address;
-    int user_data0;
-    int user_data1;
-    int user_data2;
-    float z;
-};
-
-struct PrimitiveGeometry {
-    RectWithSize local_rect;
-    RectWithSize local_clip_rect;
-};
-
-PrimitiveGeometry fetch_primitive_geometry(int address) {
-    vec4 geom[2] = fetch_from_resource_cache_2(address);
-    return PrimitiveGeometry(RectWithSize(geom[0].xy, geom[0].zw),
-                             RectWithSize(geom[1].xy, geom[1].zw));
-}
-
-Primitive load_primitive() {
-    PrimitiveInstance pi = fetch_prim_instance();
-
-    Primitive prim;
-
-    prim.scroll_node = fetch_clip_scroll_node(pi.scroll_node_id);
-    prim.clip_area = fetch_clip_area(pi.clip_task_index);
-    prim.task = fetch_picture_task(pi.render_task_index);
-
-    RectWithSize clip_chain_rect = fetch_clip_chain_rect(pi.clip_chain_rect_index);
-
-    PrimitiveGeometry geom = fetch_primitive_geometry(pi.prim_address);
-    prim.local_rect = geom.local_rect;
-    prim.local_clip_rect = intersect_rects(clip_chain_rect, geom.local_clip_rect);
-
-    prim.specific_prim_address = pi.specific_prim_address;
-    prim.user_data0 = pi.user_data0;
-    prim.user_data1 = pi.user_data1;
-    prim.user_data2 = pi.user_data2;
-    prim.z = float(pi.z);
-
-    return prim;
 }
 
 VertexInfo write_text_vertex(vec2 clamped_local_pos,
@@ -201,24 +125,30 @@ VertexInfo write_text_vertex(vec2 clamped_local_pos,
 }
 
 void main(void) {
-    Primitive prim = load_primitive();
-    TextRun text = fetch_text_run(prim.specific_prim_address);
+    int prim_header_address = aData.x;
+    int glyph_index = aData.y;
+    int resource_address = aData.z;
+    int subpx_dir = aData.w >> 16;
+    int color_mode = aData.w & 0xffff;
 
-    int glyph_index = prim.user_data0;
-    int resource_address = prim.user_data1;
-    int subpx_dir = prim.user_data2 >> 16;
-    int color_mode = prim.user_data2 & 0xffff;
+    PrimitiveHeader ph = fetch_prim_header(prim_header_address);
+
+    ClipScrollNode scroll_node = fetch_clip_scroll_node(ph.scroll_node_id);
+    ClipArea clip_area = fetch_clip_area(ph.clip_task_index);
+    PictureTask task = fetch_picture_task(ph.render_task_index);
+
+    TextRun text = fetch_text_run(ph.specific_prim_address);
 
     if (color_mode == COLOR_MODE_FROM_PASS) {
         color_mode = uMode;
     }
 
-    Glyph glyph = fetch_glyph(prim.specific_prim_address, glyph_index);
+    Glyph glyph = fetch_glyph(ph.specific_prim_address, glyph_index);
     GlyphResource res = fetch_glyph_resource(resource_address);
 
 #ifdef WR_FEATURE_GLYPH_TRANSFORM
     // Transform from local space to glyph space.
-    mat2 transform = mat2(prim.scroll_node.transform) * uDevicePixelRatio;
+    mat2 transform = mat2(scroll_node.transform) * uDevicePixelRatio;
 
     // Compute the glyph rect in glyph space.
     RectWithSize glyph_rect = RectWithSize(res.offset + transform * (text.offset + glyph.offset),
@@ -234,9 +164,9 @@ void main(void) {
     // If the glyph's local rect would fit inside the local clip rect, then select a corner from
     // the device space glyph rect to reduce overdraw of clipped pixels in the fragment shader.
     // Otherwise, fall back to clamping the glyph's local rect to the local clip rect.
-    local_pos = rect_inside_rect(local_rect, prim.local_clip_rect) ?
+    local_pos = rect_inside_rect(local_rect, ph.local_clip_rect) ?
                     inv * (glyph_rect.p0 + glyph_rect.size * aPosition.xy) :
-                    clamp_rect(local_pos, prim.local_clip_rect);
+                    clamp_rect(local_pos, ph.local_clip_rect);
 #else
     // Scale from glyph space to local space.
     float scale = res.scale / uDevicePixelRatio;
@@ -249,7 +179,7 @@ void main(void) {
     vec2 local_pos = glyph_rect.p0 + glyph_rect.size * aPosition.xy;
 
     // Clamp to the local clip rect.
-    local_pos = clamp_rect(local_pos, prim.local_clip_rect);
+    local_pos = clamp_rect(local_pos, ph.local_clip_rect);
 #endif
 
     vec2 snap_bias;
@@ -278,10 +208,10 @@ void main(void) {
     }
 
     VertexInfo vi = write_text_vertex(local_pos,
-                                      prim.local_clip_rect,
-                                      prim.z,
-                                      prim.scroll_node,
-                                      prim.task,
+                                      ph.local_clip_rect,
+                                      ph.z,
+                                      scroll_node,
+                                      task,
                                       text.offset,
                                       glyph_rect,
                                       snap_bias);
@@ -293,7 +223,7 @@ void main(void) {
     vec2 f = (vi.local_pos - glyph_rect.p0) / glyph_rect.size;
 #endif
 
-    write_clip(vi.screen_pos, prim.clip_area);
+    write_clip(vi.screen_pos, clip_area);
 
     switch (color_mode) {
         case COLOR_MODE_ALPHA:

webrender/src/device.rs
@@ -2234,6 +2234,11 @@ impl Device {
                 external: gl::RGBA,
                 pixel_type: gl::FLOAT,
             },
+            ImageFormat::RGBAI32 => FormatDesc {
+                internal: gl::RGBA32I as _,
+                external: gl::RGBA_INTEGER,
+                pixel_type: gl::INT,
+            },
             ImageFormat::RG8 => FormatDesc {
                 internal: gl::RG8 as _,
                 external: gl::RG,
@@ -2360,6 +2365,7 @@ impl<'a> UploadTarget<'a> {
             ImageFormat::BGRA8 => (self.bgra_format, 4, gl::UNSIGNED_BYTE),
             ImageFormat::RG8 => (gl::RG, 2, gl::UNSIGNED_BYTE),
             ImageFormat::RGBAF32 => (gl::RGBA, 16, gl::FLOAT),
+            ImageFormat::RGBAI32 => (gl::RGBA_INTEGER, 16, gl::INT),
         };
 
         let row_length = match chunk.stride {

commit 98f455f6379f5164e9d39468fd660a116034142e

Fix local clip rect for tiled images.
This fixes a reftest failure in Gecko that was introduced by
the patch in #2839.

Make sure that the tight local clip rect used for image and
gradient tiles also includes the local clip rect provided
by the clip-chain.

webrender/src/prim_store.rs
@@ -1687,7 +1687,9 @@ impl PrimitiveStore {
                                 // Tighten the clip rect because decomposing the repeated image can
                                 // produce primitives that are partially covering the original image
                                 // rect and we want to clip these extra parts out.
-                                let tight_clip_rect = metadata.local_clip_rect.intersection(&metadata.local_rect).unwrap();
+                                let tight_clip_rect = metadata
+                                    .combined_local_clip_rect
+                                    .intersection(&metadata.local_rect).unwrap();
 
                                 let visible_rect = compute_conservative_visible_rect(
                                     prim_run_context,
@@ -2689,7 +2691,9 @@ fn decompose_repeated_primitive(
     // Tighten the clip rect because decomposing the repeated image can
     // produce primitives that are partially covering the original image
     // rect and we want to clip these extra parts out.
-    let tight_clip_rect = metadata.local_clip_rect.intersection(&metadata.local_rect).unwrap();
+    let tight_clip_rect = metadata
+        .combined_local_clip_rect
+        .intersection(&metadata.local_rect).unwrap();
 
     let visible_rect = compute_conservative_visible_rect(
         prim_run_context,

wrench/reftests/image/reftest.list
@@ -6,3 +6,4 @@ fuzzy(1,331264) == tile-repeat-prim-or-decompose.yaml tile-repeat-prim-or-decomp
 platform(linux,mac) options(allow-mipmaps) == downscale.yaml downscale.png
 == segments.yaml segments.png
 platform(linux,mac) == yuv.yaml yuv.png
+== tiled-clip-chain.yaml tiled-clip-chain-ref.yaml

wrench/reftests/image/tiled-clip-chain-ref.yaml
@@ -1,0 +1,10 @@
+root:
+  items:
+    - image: checkerboard(2, 16, 16)
+      bounds: [10, 10, 260, 260]
+    - type: rect
+      bounds: [10, 110, 200, 200]
+      color: white
+    - type: rect
+      bounds: [110, 10, 200, 270]
+      color: white

wrench/reftests/image/tiled-clip-chain.yaml
@@ -1,0 +1,10 @@
+# Test that local clip rects from clip-chains are correctly
+# propagated into the local clip rect for tiled images.
+root:
+  items:
+    - type: clip
+      bounds: [10, 10, 100, 100]
+      items:
+        - image: checkerboard(2, 16, 16)
+          bounds: [10, 10, 260, 260]
+          tile-size: 64