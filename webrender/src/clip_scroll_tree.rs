371c8196a44aa7e40b8bbc476f111d5e664dd8f0
Nicolas Silva <nical@fastmail.com>
2018-09-13 11:07:20 +0200

    Avoid allocating small vectors when updating the clip scroll tree.

--- webrender/src/clip_scroll_tree.rs
+++ webrender/src/clip_scroll_tree.rs
@@ -291,7 +291,9 @@ impl ClipScrollTree {
         // TODO(gw): This is an ugly borrow check workaround to clone these.
         //           Restructure this to avoid the clones!
         let mut state = state.clone();
-        let node_children = {
+        let mut node_children: SmallVec<[SpatialNodeIndex; 4]> = SmallVec::new();
+
+        {
             let node = match self.spatial_nodes.get_mut(node_index.0) {
                 Some(node) => node,
                 None => return,
@@ -307,8 +309,8 @@ impl ClipScrollTree {
             }
 
             node.prepare_state_for_children(&mut state);
-            node.children.clone()
-        };
+            node_children.extend_from_slice(&node.children);
+        }
 
         for child_node_index in node_children {
             self.update_node(
