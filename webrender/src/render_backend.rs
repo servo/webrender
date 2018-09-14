62d8183c6fc8edfcd01621e4ef2a2978764fb75e
Dzmitry Malyshau <kvark@mozilla.com>
2018-09-13 21:20:25 -0400

    Revert "Avoid re-building the frame when we only need the hit tester."
    This reverts commit a30cf48861ebf3efbe73f58b6df1592270b5fa73.

--- webrender/src/render_backend.rs
+++ webrender/src/render_backend.rs
@@ -350,12 +350,14 @@ impl Document {
 
 struct DocumentOps {
     scroll: bool,
+    build_frame: bool,
 }
 
 impl DocumentOps {
     fn nop() -> Self {
         DocumentOps {
             scroll: false,
+            build_frame: false,
         }
     }
 }
@@ -615,6 +617,7 @@ impl RenderBackend {
                             replace(&mut txn.resource_updates, Vec::new()),
                             replace(&mut txn.frame_ops, Vec::new()),
                             replace(&mut txn.notifications, Vec::new()),
+                            txn.build_frame,
                             txn.render_frame,
                             &mut frame_counter,
                             &mut profile_counters,
@@ -870,6 +873,7 @@ impl RenderBackend {
             rasterized_blobs: Vec::new(),
             notifications: transaction_msg.notifications,
             set_root_pipeline: None,
+            build_frame: transaction_msg.generate_frame,
             render_frame: transaction_msg.generate_frame,
         });
 
@@ -904,6 +908,7 @@ impl RenderBackend {
                 replace(&mut txn.resource_updates, Vec::new()),
                 replace(&mut txn.frame_ops, Vec::new()),
                 replace(&mut txn.notifications, Vec::new()),
+                txn.build_frame,
                 txn.render_frame,
                 frame_counter,
                 profile_counters,
@@ -940,6 +945,7 @@ impl RenderBackend {
         resource_updates: Vec<ResourceUpdate>,
         mut frame_ops: Vec<FrameMsg>,
         mut notifications: Vec<NotificationRequest>,
+        mut build_frame: bool,
         mut render_frame: bool,
         frame_counter: &mut u32,
         profile_counters: &mut BackendProfileCounters,
@@ -952,7 +958,7 @@ impl RenderBackend {
         // fiddle with things after a potentially long scene build, but just
         // before rendering. This is useful for rendering with the latest
         // async transforms.
-        if requested_frame {
+        if build_frame {
             if let Some(ref sampler) = self.sampler {
                 frame_ops.append(&mut sampler.sample());
             }
@@ -966,6 +972,7 @@ impl RenderBackend {
         for frame_msg in frame_ops {
             let _timer = profile_counters.total_time.timer();
             let op = doc.process_frame_msg(frame_msg);
+            build_frame |= op.build_frame;
             scroll |= op.scroll;
         }
 
@@ -980,23 +987,30 @@ impl RenderBackend {
             &mut profile_counters.resources,
         );
 
+        // After applying the new scene we need to
+        // rebuild the hit-tester, so we trigger a frame generation
+        // step.
+        //
+        // TODO: We could avoid some the cost of building the frame by only
+        // building the information required for hit-testing (See #2807).
+        build_frame |= has_built_scene;
+
         if doc.dynamic_properties.flush_pending_updates() {
             doc.frame_is_valid = false;
             doc.hit_tester_is_valid = false;
+            build_frame = true;
         }
 
         if !doc.can_render() {
             // TODO: this happens if we are building the first scene asynchronously and
             // scroll at the same time. we should keep track of the fact that we skipped
             // composition here and do it as soon as we receive the scene.
+            build_frame = false;
             render_frame = false;
         }
 
-        // Avoid re-building the frame if the current built frame is still valid.
-        let build_frame = render_frame && !doc.frame_is_valid;
-
-        if !doc.hit_tester_is_valid && !build_frame {
-            doc.rebuild_hit_tester();
+        if doc.frame_is_valid {
+            build_frame = false;
         }
 
         let mut frame_build_time = None;
