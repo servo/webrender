53d19220cb58d6a7516c9e1f8e4981dd4020184c
agoloman <agoloman@mozilla.com>
2026-05-13 13:45:11 +0300

    Revert "Bug 2033393 - Adapt consumers to http 1.x, hyper 1.x, and windows-sys HANDLE type changes. r=necko-reviewers,geckodriver-reviewers,gsvelto,smaug,jgraham,gw" for causing mochitest failures.
    This reverts commit dad44d76c34e9b20811112612347ba38558832f5.
    
    Revert "Bug 2033393 - Vendor upgraded http, hyper, warp crates and their dependencies. r=supply-chain-reviewers,webrtc-reviewers,geckodriver-reviewers,jdescottes,jgraham,bwc,valentin,afranchuk,gsvelto,gw,padenot"
    
    This reverts commit dce6afb41f4e851993c5edad1397b09a265ed9ae.
    
    Revert "Bug 2033393 - Refresh expired wildcard audits. r=supply-chain-reviewers,emilio"
    
    This reverts commit df3a2c86d64ca33b5ccab65baa9f34ab32ce0e63.

--- webrender/src/debugger.rs
+++ webrender/src/debugger.rs
@@ -15,13 +15,8 @@ use api::debugger::{CompositorDebugInfo, CompositorDebugTile};
 use std::thread;
 use base64::prelude::*;
 use sha1::{Sha1, Digest};
-use hyper::{Request, Response, body::Incoming, service::service_fn};
-use hyper_util::rt::TokioIo;
-use hyper_util::server::conn::auto::Builder as ServerBuilder;
-use http_body_util::{BodyExt, Full};
-use hyper::body::Bytes;
+use hyper::{Request, Response, Body, service::{make_service_fn, service_fn}, Server};
 use tokio::io::AsyncWriteExt;
-use tokio::net::TcpListener;
 
 /// A minimal wrapper around RenderApi's channel that can be cloned.
 #[derive(Clone)]
@@ -194,58 +189,48 @@ pub fn start(api: RenderApi) {
         };
 
         runtime.block_on(async {
-            let listener = match TcpListener::bind(address).await {
-                Ok(l) => l,
+            let make_svc = make_service_fn(move |_conn| {
+                let api = api.clone();
+                async move {
+                    Ok::<_, Infallible>(service_fn(move |req| {
+                        handle_request(req, api.clone())
+                    }))
+                }
+            });
+
+            let addr = address.parse().unwrap();
+            let server = match Server::try_bind(&addr) {
+                Ok(s) => s,
                 Err(e) => {
-                    eprintln!("WebRender debugger could not bind: {address}: {e:?}");
+                    eprintln!("WebRender debugger could not bind: {addr}: {e:?}");
                     return;
                 }
             };
 
-            loop {
-                let (stream, _) = match listener.accept().await {
-                    Ok(conn) => conn,
-                    Err(e) => {
-                        eprintln!("WebRender debugger accept error: {:?}", e);
-                        continue;
-                    }
-                };
-
-                let api = api.clone();
-                tokio::spawn(async move {
-                    let io = TokioIo::new(stream);
-                    let svc = service_fn(move |req| {
-                        handle_request(req, api.clone())
-                    });
-                    if let Err(e) = ServerBuilder::new(hyper_util::rt::TokioExecutor::new())
-                        .serve_connection_with_upgrades(io, svc)
-                        .await
-                    {
-                        eprintln!("WebRender debugger connection error: {:?}", e);
-                    }
-                });
+            if let Err(e) = server.serve(make_svc).await {
+                eprintln!("WebRender debugger error: {:?}", e);
             }
         });
     });
 }
 
-async fn request_to_string(request: Request<Incoming>) -> Result<String, hyper::Error> {
-    let body_bytes = request.into_body().collect().await?.to_bytes();
+async fn request_to_string(request: Request<Body>) -> Result<String, hyper::Error> {
+    let body_bytes = hyper::body::to_bytes(request.into_body()).await?;
     Ok(String::from_utf8_lossy(&body_bytes).to_string())
 }
 
-fn string_response<S: Into<String>>(string: S) -> Response<Full<Bytes>> {
-    Response::new(Full::new(Bytes::from(string.into())))
+fn string_response<S: Into<String>>(string: S) -> Response<Body> {
+    Response::new(Body::from(string.into()))
 }
 
-fn status_response(status: u16) -> Response<Full<Bytes>> {
-    Response::builder().status(status).body(Full::new(Bytes::new())).unwrap()
+fn status_response(status: u16) -> Response<Body> {
+    Response::builder().status(status).body(Body::from("")).unwrap()
 }
 
 async fn handle_request(
-    request: Request<Incoming>,
+    request: Request<Body>,
     api: DebugRenderApi,
-) -> Result<Response<Full<Bytes>>, Infallible> {
+) -> Result<Response<Body>, Infallible> {
     let path = request.uri().path();
     let query = request.uri().query().unwrap_or("");
     let args: HashMap<String, String> = url::form_urlencoded::parse(query.as_bytes())
@@ -358,7 +343,7 @@ async fn handle_request(
 
                         // Spawn a task to handle writing to the WebSocket stream
                         tokio::spawn(async move {
-                            let mut stream = TokioIo::new(upgraded);
+                            let mut stream = upgraded;
                             while let Some(data) = rx.recv().await {
                                 if stream.write_all(&data).await.is_err() {
                                     break;
@@ -384,7 +369,7 @@ async fn handle_request(
                 .header("upgrade", "websocket")
                 .header("connection", "upgrade")
                 .header("sec-websocket-accept", accept_key)
-                .body(Full::new(Bytes::new()))
+                .body(Body::from(""))
                 .unwrap())
         }
         _ => {
