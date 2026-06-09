759bb0e4c242cdeb3588ee064c46b93444a8172f
Sotaro Ikeda <sotaro.ikeda.g@gmail.com>
2026-06-09 23:45:14 +0000

    Bug 2045177 - Allow more underlays if forced in TileCacheInstance::can_promote_to_surface() r=gfx-reviewers,bradwerth
    gecko permits only one underlay at a time. Current gecko does not support full HDR yet. HDR video without underlay(overlay) causes color change.
    
    Differential Revision: https://phabricator.services.mozilla.com/D304940

--- webrender/src/tile_cache/mod.rs
+++ webrender/src/tile_cache/mod.rs
@@ -65,6 +65,10 @@ pub use api::units::TileRange as TileRect;
 /// prevent performance and memory usage drastically degrading in pathological cases.
 pub const MAX_COMPOSITOR_SURFACES: usize = 4;
 
+/// The maximum number of compositor underlay surfaces that are allowed per picture cache.
+/// This is an arbitrary number that should be enough for most cases.
+pub const MAX_COMPOSITOR_UNDERLAY_SURFACES: usize = 5;
+
 /// The size in device pixels of a normal cached tile.
 pub const TILE_SIZE_DEFAULT: DeviceIntSize = DeviceIntSize {
     width: 1024,
@@ -1533,7 +1537,7 @@ impl TileCacheInstance {
         data_stores: &DataStores,
         clip_store: &ClipStore,
         composite_state: &CompositeState,
-        force: bool,
+        color_depth: Option<ColorDepth>,
     ) -> Result<CompositorSurfaceKind, SurfacePromotionFailure> {
         use SurfacePromotionFailure::*;
 
@@ -1582,6 +1586,10 @@ impl TileCacheInstance {
                 }
             }
             CompositorSurfaceKind::Underlay => {
+                // Should we force the promotion of this surface? We'll force it if promotion
+                // is necessary for correct color display of HDR.
+                let force_for_hdr = matches!(color_depth, Some(color_depth) if color_depth.bit_depth() > 8);
+
                 // If a mask is needed, there are some restrictions.
                 if prim_clip_chain.needs_mask {
                     // Need an opaque region behind this prim. The opaque region doesn't
@@ -1590,7 +1598,7 @@ impl TileCacheInstance {
                     if !self.backdrop.opaque_rect.contains_box(&pic_coverage_rect) {
                         let result = Err(UnderlayAlphaBackdrop);
                         // If we aren't forcing, give up and return Err.
-                        if !force {
+                        if !force_for_hdr {
                             return result;
                         }
 
@@ -1598,9 +1606,14 @@ impl TileCacheInstance {
                         self.report_promotion_failure(result, pic_coverage_rect, true);
                     }
 
-                    // Only one masked underlay allowed.
                     if !self.underlays.is_empty() {
-                        return Err(UnderlaySurfaceLimit);
+                        // If we aren't forcing, we limit the number of masked underlays
+                        // Permit more underlays when forced.
+                        // XXX WebRender does not support full HDR yet.
+                        // HDR requires external composite to show correct colors.
+                        if !force_for_hdr || self.underlays.len() > MAX_COMPOSITOR_UNDERLAY_SURFACES {
+                            return Err(UnderlaySurfaceLimit);
+                        }
                     }
                 }
 
@@ -1609,7 +1622,7 @@ impl TileCacheInstance {
                 if self.overlay_region.intersects(&pic_coverage_rect) {
                     let result = Err(UnderlayIntersectsOverlay);
                     // If we aren't forcing, give up and return Err.
-                    if !force {
+                    if !force_for_hdr {
                         return result;
                     }
 
@@ -2381,7 +2394,7 @@ impl TileCacheInstance {
                                                           data_stores,
                                                           clip_store,
                                                           composite_state,
-                                                          false);
+                                                          None);
                     }
 
                     // Native OS compositors (DC and CA, at least) support premultiplied alpha
@@ -2457,10 +2470,6 @@ impl TileCacheInstance {
                         self.yuv_images_remaining -= 1;
                     }
 
-                    // Should we force the promotion of this surface? We'll force it if promotion
-                    // is necessary for correct color display.
-                    let force = prim_data.kind.color_depth.bit_depth() > 8;
-
                     let promotion_attempts =
                         [CompositorSurfaceKind::Overlay, CompositorSurfaceKind::Underlay];
 
@@ -2478,7 +2487,7 @@ impl TileCacheInstance {
                                                     data_stores,
                                                     clip_store,
                                                     composite_state,
-                                                    force);
+                                                    Some(prim_data.kind.color_depth));
                         if promotion_result.is_ok() {
                             break;
                         }
