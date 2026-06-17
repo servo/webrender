baa839149db2c05e8f7f29f867c6ff294ff0c2ce
agoloman <agoloman@mozilla.com>
2026-06-17 14:03:42 +0300

    Revert "Bug 2047627 - Refactor BorderInstance data into gpu buffer. r=nical,gfx-reviewers,gw" for causing webrender bustages @clip-superellipse.yaml.
    This reverts commit 066fb2e0383f7cac0838fee4ea7216c4b13b9ec9.
    
    Revert "Bug 2047627 - Add yaml parsing of corner shape in wrench with some reftests. r=gfx-reviewers,nical"
    
    This reverts commit 3ab4252184ad322cca58ff13cb5c1ce456d8a70a.
    
    Revert "Bug 2047627 - Add debug helper for signed distance fields. r=gfx-reviewers,nical"
    
    This reverts commit 59c01ad38a97a9fa463dc66341dc5da6e43698cb.
    
    Revert "Bug 2047627 - Add support for isnan() and isinf() in SWGL. r=gfx-reviewers,nical,lsalzman"
    
    This reverts commit eaa35ec30c8e3167f66c762c75af1b659ba338eb.
    
    Revert "Bug 2047627 - Add operator += to vec2_scalar_ref in SWGL. r=gfx-reviewers,nical"
    
    This reverts commit 2b05135af93013902544bb367a59f4d22d13a49b.
    
    Revert "Bug 2047627 - Add support for vec3 *= float in SWGL. r=gfx-reviewers,nical"
    
    This reverts commit bac8577fd78a2ab3159de8189302f9f790eb5f06.
    
    Revert "Bug 2047627 - Add support for constructor vec3(float, vec2) in SWGL. r=gfx-reviewers,nical"
    
    This reverts commit ad85613e67a24ab4a1c8f814f0f8fba28cd2dcda.

--- webrender/src/prim_store/borders.rs
+++ webrender/src/prim_store/borders.rs
@@ -232,7 +232,7 @@ impl NormalBorderData {
                 &mut frame_state.frame_gpu_data.f32,
                 frame_state.rg_builder,
                 &mut frame_state.surface_builder,
-                &mut |rg_builder, gpu_buffer_builder| {
+                &mut |rg_builder, _| {
                     rg_builder.add().init(RenderTask::new_dynamic(
                         cache_size,
                         RenderTaskKind::new_border_segment(
@@ -241,7 +241,6 @@ impl NormalBorderData {
                                 cache_size,
                                 &self.border,
                                 scale,
-                                gpu_buffer_builder,
                             )
                         ),
                     ))
